ad636a4aa7c4e70cb0118c3374ac6b05cf273511
Bence Cserna <bence@cserna.net>
2015-12-04 18:45:36 -0500

    Add SLF4J logging

---
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/experiment/Experiment.kt
@@ -0,0 +1,8 @@
+package edu.unh.cs.ai.realtimesearch.experiment
+
+/**
+ * @author Bence Cserna (bence@cserna.net)
+ */
+interface Experiment {
+    fun run()
+}
---
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/experiment/RTSExperiment.kt
@@ -0,0 +1,14 @@
+package edu.unh.cs.ai.realtimesearch.experiment
+
+import org.slf4j.LoggerFactory
+
+/**
+ * @author Bence Cserna (bence@cserna.net)
+ */
+class RTSExperiment : Experiment {
+    val logger = LoggerFactory.getLogger("RTSExperiment")
+
+    override fun run() {
+        throw UnsupportedOperationException()
+    }
+}
