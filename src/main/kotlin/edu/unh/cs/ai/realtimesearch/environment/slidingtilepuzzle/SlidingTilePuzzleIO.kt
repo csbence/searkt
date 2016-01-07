e3b51c2afcbe013705cd7b1b05a199fb3632a9ed
Bence Cserna <bence@cserna.net>
2016-01-07 13:34:09 +0100

    Add legacy sliding tile puzzle file parser

---
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/slidingtilepuzzle/SlidingTilePuzzleIO.kt
@@ -0,0 +1,57 @@
+package edu.unh.cs.ai.realtimesearch.environment.slidingtilepuzzle
+
+import java.io.InputStream
+import java.util.*
+
+/**
+ * @author Bence Cserna (bence@cserna.net)
+ */
+object SlidingTilePuzzleIO {
+
+    fun parseFromStream(input: InputStream): SlidingTilePuzzleInstance {
+        val inputScanner = Scanner(input)
+        inputScanner.useDelimiter("\n")
+
+        val dimension: Int
+
+        try {
+            val dimensions = inputScanner.nextLine().trim().split(" ").map { it.toInt() }
+
+            if (dimensions.size != 2) {
+                throw InvalidSlidingTilePuzzleException("Only two dimensional puzzles are supported.")
+            }
+
+            if (dimensions[0] != dimensions[1]) {
+                throw InvalidSlidingTilePuzzleException("Only square puzzles are supported.")
+            }
+
+            dimension = dimensions[0]
+        } catch (e: NoSuchElementException) {
+            throw InvalidSlidingTilePuzzleException("SlidingTilePuzzle's first line is missing. The first line should contain the dimensions", e)
+        } catch (e: NumberFormatException) {
+            throw InvalidSlidingTilePuzzleException("SlidingTilePuzzle's dimensions must be numbers.", e)
+        }
+
+        val slidingTilePuzzle = SlidingTilePuzzle(dimension)
+        val puzzle = Array(dimension, { ByteArray(dimension) })
+
+        try {
+            val tileList = inputScanner.asSequence().drop(1).take(dimension * dimension).map { it.toInt().toByte() }.toList()
+            var zeroLocation: SlidingTilePuzzleState.Location? = null
+            tileList.forEachIndexed { i, value ->
+                puzzle[i % dimension][i / dimension] = value
+                if (value == 0.toByte()) {
+                    zeroLocation = SlidingTilePuzzleState.Location(i % dimension, i / dimension)
+                }
+            }
+
+            return SlidingTilePuzzleInstance(slidingTilePuzzle, SlidingTilePuzzleState(zeroLocation!!, puzzle, slidingTilePuzzle.heuristic(puzzle)))
+        } catch (e: NumberFormatException) {
+            throw InvalidSlidingTilePuzzleException("Tile must be a number.", e)
+        }
+    }
+
+}
+
+class InvalidSlidingTilePuzzleException(message: String, e: Exception? = null) : RuntimeException(message, e)
+data class SlidingTilePuzzleInstance(val slidingTilePuzzle: SlidingTilePuzzle, val startState: SlidingTilePuzzleState)
