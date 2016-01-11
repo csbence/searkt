f0dd7b634caabdb2e5f7d4ffd9a7a06548789fb7
Bence Cserna <bence@cserna.net>
2016-01-11 14:12:48 +0100

    Add Sliding Tile Puzzle A* tests

--- src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/slidingtilepuzzle/SlidingTilePuzzleTest.kt
+++ src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/slidingtilepuzzle/SlidingTilePuzzleTest.kt
@@ -1,6 +1,11 @@
 package edu.unh.cs.ai.realtimesearch.environment.slidingtilepuzzle
 
+import edu.unh.cs.ai.realtimesearch.agent.ClassicalAgent
+import edu.unh.cs.ai.realtimesearch.experiment.ClassicalExperiment
+import edu.unh.cs.ai.realtimesearch.planner.classical.closedlist.heuristic.AStarPlanner
 import org.junit.Test
+import java.io.File
+import java.io.FileInputStream
 import kotlin.test.assertTrue
 
 /**
@@ -10,7 +15,7 @@ class SlidingTilePuzzleTest {
 
     @Test
     fun testGoalHeuristic() {
-        val tiles = tiles(8) {
+        val tiles = tiles(3) {
             row (0, 1, 2)
             row (3, 4, 5)
             row (6, 7, 8)
@@ -23,7 +28,7 @@ class SlidingTilePuzzleTest {
 
     @Test
     fun testHeuristic1() {
-        val tiles = tiles(8) {
+        val tiles = tiles(3) {
             row (1, 0, 2)
             row (3, 4, 5)
             row (6, 7, 8)
@@ -36,7 +41,7 @@ class SlidingTilePuzzleTest {
 
     @Test
     fun testHeuristic2() {
-        val tiles = tiles(8) {
+        val tiles = tiles(3) {
             row (3, 1, 2)
             row (0, 4, 5)
             row (6, 7, 8)
@@ -49,7 +54,7 @@ class SlidingTilePuzzleTest {
 
     @Test
     fun testHeuristic3() {
-        val tiles = tiles(8) {
+        val tiles = tiles(3) {
             row (1, 2, 0)
             row (4, 3, 8)
             row (7, 6, 5)
@@ -58,6 +63,89 @@ class SlidingTilePuzzleTest {
         val slidingTilePuzzle = SlidingTilePuzzle(3)
         val heuristic = slidingTilePuzzle.heuristic(tiles)
         assertTrue { heuristic == 8.0 }
+    }
+
+    @Test
+    fun testSuccessors1() {
+        val tiles = tiles(3) {
+            row (1, 2, 0)
+            row (4, 3, 8)
+            row (7, 6, 5)
+        }
+
+        val slidingTilePuzzle = SlidingTilePuzzle(3)
+        val heuristic = slidingTilePuzzle.heuristic(tiles)
+
+        val state = SlidingTilePuzzleState(SlidingTilePuzzleState.Location(2, 0), tiles, heuristic)
+
+        val successors = slidingTilePuzzle.successors(state)
+
+        assertTrue(successors.size == 2)
+    }
+
+    @Test
+    fun testAStar1() {
+        val tiles = tiles(3) {
+            row (5, 6, 8)
+            row (4, 3, 1)
+            row (2, 7, 0)
+        }
+
+        val slidingTilePuzzle = SlidingTilePuzzle(3)
+        val initialState = SlidingTilePuzzleState(SlidingTilePuzzleState.Location(2, 2), tiles, slidingTilePuzzle.heuristic(tiles))
+
+        val aStarAgent = ClassicalAgent(AStarPlanner(slidingTilePuzzle))
+        val aStarExperiment = ClassicalExperiment(aStarAgent, slidingTilePuzzle, initialState, 1)
+
+        aStarExperiment.run()
+    }
+
+    @Test
+    fun testAStar2() {
+        val tiles = tiles(3) {
+            row (1, 0, 2)
+            row (3, 4, 5)
+            row (6, 7, 8)
+        }
+
+        val slidingTilePuzzle = SlidingTilePuzzle(3)
+        val initialState = SlidingTilePuzzleState(SlidingTilePuzzleState.Location(1, 0), tiles, slidingTilePuzzle.heuristic(tiles))
+
+        val aStarAgent = ClassicalAgent(AStarPlanner(slidingTilePuzzle))
+        val aStarExperiment = ClassicalExperiment(aStarAgent, slidingTilePuzzle, initialState, 1)
+
+        aStarExperiment.run()
+    }
+
+    @Test
+    fun testAStar3() {
+        val tiles = tiles(3) {
+            row (1, 2, 0)
+            row (3, 4, 5)
+            row (6, 7, 8)
+        }
+
+        val slidingTilePuzzle = SlidingTilePuzzle(3)
+        val initialState = SlidingTilePuzzleState(SlidingTilePuzzleState.Location(2, 0), tiles, slidingTilePuzzle.heuristic(tiles))
+
+        val aStarAgent = ClassicalAgent(AStarPlanner(slidingTilePuzzle))
+        val aStarExperiment = ClassicalExperiment(aStarAgent, slidingTilePuzzle, initialState, 1)
+
+        aStarExperiment.run()
+    }
+
+    @Test
+    fun testAStarFromFile() {
+        Thread.sleep(10000)
+
+        val slidingTilePuzzleInstance = SlidingTilePuzzleIO.parseFromStream(FileInputStream(File("input/tiles/korf/5/1")))
+        val slidingTilePuzzle = slidingTilePuzzleInstance.slidingTilePuzzle
+        val initialState = slidingTilePuzzleInstance.startState
+
+        val aStarAgent = ClassicalAgent(AStarPlanner(slidingTilePuzzle))
+        val aStarExperiment = ClassicalExperiment(aStarAgent, slidingTilePuzzle, initialState, 1)
+
+        aStarExperiment.run()
     }
 
     private fun tiles(size: Int, init: Array<ByteArray>.() -> Unit): Array<ByteArray> {
