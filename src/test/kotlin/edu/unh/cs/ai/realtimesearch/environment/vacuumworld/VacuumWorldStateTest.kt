5c916b0dbd981dd916a42f66ca4ec606402f6bed
Bence Cserna <bence@cserna.net>
2016-01-14 10:26:16 +0100

    Update Kotlin version and dependencies

--- src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/vacuumworld/VacuumWorldStateTest.kt
+++ src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/vacuumworld/VacuumWorldStateTest.kt
@@ -1,5 +1,6 @@
 import edu.unh.cs.ai.realtimesearch.environment.vacuumworld.VacuumWorldState
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class VacuumWorldStateTest {
 
@@ -11,15 +12,15 @@ class VacuumWorldStateTest {
         val l4 = VacuumWorldState.Location(3, 10)
         val l5 = VacuumWorldState.Location(100, 234522)
 
-        assert(true)
-        assert(l1 + l2 == VacuumWorldState.Location(l1.x + l2.x, l1.y+l2.y))
-        assert(l2 + l2 == VacuumWorldState.Location(l2.x + l2.x, l2.y+l2.y))
-        assert(l1 + l3 == VacuumWorldState.Location(l1.x + l3.x, l1.y+l3.y))
-        assert(l4 + l5 == VacuumWorldState.Location(l4.x + l5.x, l4.y+l5.y))
-        assert(l3 + l2 == VacuumWorldState.Location(l3.x + l2.x, l3.y+l2.y))
-        assert(l3 + l1 == VacuumWorldState.Location(l3.x + l1.x, l3.y+l1.y))
-        assert(l5 + l2 == VacuumWorldState.Location(l5.x + l2.x, l5.y+l2.y))
-        assert(l2 + l5 == VacuumWorldState.Location(l2.x + l5.x, l2.y+l5.y))
+        assertTrue(true)
+        assertTrue(l1 + l2 == VacuumWorldState.Location(l1.x + l2.x, l1.y+l2.y))
+        assertTrue(l2 + l2 == VacuumWorldState.Location(l2.x + l2.x, l2.y+l2.y))
+        assertTrue(l1 + l3 == VacuumWorldState.Location(l1.x + l3.x, l1.y+l3.y))
+        assertTrue(l4 + l5 == VacuumWorldState.Location(l4.x + l5.x, l4.y+l5.y))
+        assertTrue(l3 + l2 == VacuumWorldState.Location(l3.x + l2.x, l3.y+l2.y))
+        assertTrue(l3 + l1 == VacuumWorldState.Location(l3.x + l1.x, l3.y+l1.y))
+        assertTrue(l5 + l2 == VacuumWorldState.Location(l5.x + l2.x, l5.y+l2.y))
+        assertTrue(l2 + l5 == VacuumWorldState.Location(l2.x + l5.x, l2.y+l5.y))
     }
 
 }
