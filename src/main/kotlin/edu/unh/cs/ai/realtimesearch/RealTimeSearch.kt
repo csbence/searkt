eeaafcaa2b99b732a6fb4807181b5c3ecef00b56
Bence Cserna <bence@cserna.net>
2016-01-14 10:27:46 +0100

    Add generics to main

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
@@ -1,13 +1,18 @@
 package edu.unh.cs.ai.realtimesearch
 
 import edu.unh.cs.ai.realtimesearch.agent.ClassicalAgent
-import edu.unh.cs.ai.realtimesearch.environment.vacuumworld.VacuumWorld
-import edu.unh.cs.ai.realtimesearch.environment.vacuumworld.VacuumWorldEnvironment
-import edu.unh.cs.ai.realtimesearch.environment.vacuumworld.VacuumWorldState
+import edu.unh.cs.ai.realtimesearch.agent.RTSAgent
+import edu.unh.cs.ai.realtimesearch.environment.slidingtilepuzzle.SlidingTilePuzzleEnvironment
+import edu.unh.cs.ai.realtimesearch.environment.slidingtilepuzzle.SlidingTilePuzzleIO
+import edu.unh.cs.ai.realtimesearch.environment.slidingtilepuzzle.SlidingTilePuzzleState
 import edu.unh.cs.ai.realtimesearch.experiment.ClassicalExperiment
 import edu.unh.cs.ai.realtimesearch.experiment.ExperimentResult
+import edu.unh.cs.ai.realtimesearch.experiment.RTSExperiment
 import edu.unh.cs.ai.realtimesearch.experiment.terminationCheckers.CallsTerminationChecker
 import edu.unh.cs.ai.realtimesearch.planner.classical.closedlist.heuristic.AStarPlanner
+import edu.unh.cs.ai.realtimesearch.planner.realtime_.LssLrtaStarPlanner
+import java.io.File
+import java.io.FileInputStream
 import java.io.PrintWriter
 import java.util.*
 
@@ -18,31 +23,31 @@ fun main(args: Array<String>) {
     // parameters / settings
 
     // init
-    val world = VacuumWorld(3, 3, arrayListOf(
-    ), 2)
+//    val world = VacuumWorld(3, 3, arrayListOf(
+//    ), 2)
+//
+//    val state = VacuumWorldState(VacuumWorldState.Location(0, 0), setOf(
+//            VacuumWorldState.Location(2, 0),
+//            VacuumWorldState.Location(1, 1)
+//    ))
 
-    val state = VacuumWorldState(VacuumWorldState.Location(0, 0), setOf(
-            VacuumWorldState.Location(2, 0),
-            VacuumWorldState.Location(1, 1)
-    ))
+    val slidingTilePuzzleInstance = SlidingTilePuzzleIO.parseFromStream(FileInputStream(File("input/tiles/korf/5/1")))
+    val world = slidingTilePuzzleInstance.slidingTilePuzzle
+    val state = slidingTilePuzzleInstance.startState
 
-    val vacuumEnvironment = VacuumWorldEnvironment(world, state)
-    val randomVacuumEnvironment = VacuumWorldEnvironment(world)
-
-    //val terminalCondition = TimeTerminationChecker(10.0)
-    //val terminalCondition = FakeTerminationChecker()
+    val environment = SlidingTilePuzzleEnvironment(world, state)
     val terminalCondition = CallsTerminationChecker(10)
 
-    val aStarAgent = ClassicalAgent(AStarPlanner(world))
-    //val lssRTAAgent = RTSAgent(LSSLRTAStarPlanner(world))
+    val aStarAgent = ClassicalAgent<SlidingTilePuzzleState>(AStarPlanner(world))
+    val lssRTAAgent = RTSAgent<SlidingTilePuzzleState>(LssLrtaStarPlanner(world))
 
-    val aStarExperiment = ClassicalExperiment(aStarAgent, world, null, 10)
-    //val lssRTAExperiment = RTSExperiment(lssRTAAgent, vacuumEnvironment, terminalCondition)
+    val aStarExperiment = ClassicalExperiment(aStarAgent, world, state, 10)
+    val lssRTAExperiment = RTSExperiment(lssRTAAgent, environment, terminalCondition)
     //val randomLssRTAExperiment = RTSExperiment(lssRTAAgent, randomVacuumEnvironment, terminalCondition)
 
     // run experiment
     print("A*\n")
-    val results = aStarExperiment.run()
+    val results = lssRTAExperiment.run()
     writeResultsToFile("AStar", results)
     //print("LSS RTA*\n")
 //    val lssResults = lssRTAExperiment.run()
