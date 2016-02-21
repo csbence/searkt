59e3a3ac2a70d70357207e0f892f7a1ead5afed6
Stephen Chambers <stephenchambers515@gmail.com>
2016-02-21 14:06:26 -0500

    The robot now goes at a constant speed, and the speed is relative to the length of the path. Also fixed some math errors that caused the robot to not be in the center of the square. Finally, fixed the resizing issue that wrecked the world.

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/VaccumVisualizer.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/VaccumVisualizer.kt
@@ -7,21 +7,23 @@ package edu.unh.cs.ai.realtimesearch.visualizer
 import javafx.animation.PathTransition
 import javafx.animation.Timeline
 import javafx.application.Application
+import javafx.animation.Interpolator
 import javafx.scene.Scene
 import javafx.stage.Stage
 import javafx.scene.paint.Color
 import java.util.*
-import javafx.scene.layout.TilePane
+import javafx.scene.layout.Pane
 import javafx.scene.shape.*
 import javafx.util.Duration
 import kotlin.system.exitProcess
 
 class VaccumVisualizer : Application() {
     override fun start(primaryStage: Stage) {
+
         /* Get domain from Application */
         val parameters = getParameters()!!
         val raw = parameters.getRaw()!!
-        if(raw.isEmpty()){
+        if (raw.isEmpty()) {
             println("Cannot visualize without a domain!")
             exitProcess(1)
         }
@@ -29,10 +31,10 @@ class VaccumVisualizer : Application() {
 
         /* Get action list from Application */
         val actionList: MutableList<String> = arrayListOf()
-        for (i in 1..raw.size - 1){
+        for (i in 1..raw.size - 1) {
             actionList.add(raw.get(i))
         }
-        println(actionList)
+        val TIME_TO_RUN = actionList.size * 200.0
 
         /* Assuming the domain is correct because the experiment was already run */
         primaryStage.title = "RTS Visualizer"
@@ -44,22 +46,18 @@ class VaccumVisualizer : Application() {
         columnCount = inputScanner.nextLine().toInt()
         rowCount = inputScanner.nextLine().toInt()
 
-        /* Graphical parameters */
-        val WIDTH = 1200.0
-        val HEIGHT = 700.0
-        val TILE_WIDTH: Double = (WIDTH/columnCount)
-        println(WIDTH)
-        println(TILE_WIDTH)
-        val TILE_HEIGHT: Double = (HEIGHT/rowCount)
-        println(HEIGHT)
-        println(TILE_HEIGHT)
+        val root = Pane()
 
-        val root = TilePane(0.0, 0.0)
-        root.isSnapToPixel = true
+        /* Graphical parameters */
+        val WIDTH = 1700.0
+        val HEIGHT = 700.0
+        val TILE_WIDTH: Double = (WIDTH / columnCount)
+        val TILE_HEIGHT: Double = (HEIGHT / rowCount)
+        val TILE_SIZE = Math.min(TILE_WIDTH, TILE_HEIGHT)
+
         /* The robot */
-        val robotWidth = TILE_WIDTH/4.0
+        val robotWidth = TILE_SIZE / 2.0
         val robot = Rectangle(robotWidth, robotWidth)
-        //robot.stroke = Color.ORANGE
         robot.fill = Color.ORANGE
 
         /* The robots starting location, needs to be drawn later */
@@ -73,21 +71,36 @@ class VaccumVisualizer : Application() {
             for (x in 0..columnCount - 1) {
                 when (line[x]) {
                     '#' -> {
-                        val blocked = Rectangle(TILE_WIDTH, TILE_HEIGHT)
+                        val blocked = Rectangle(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE)
                         blocked.fill = Color.BLACK
-                        //blocked.stroke = Color.BLACK
+                        blocked.stroke = Color.BLACK
+                        /*blocked.widthProperty().bind(root.widthProperty().divide(columnCount))
+                        blocked.heightProperty().bind(root.heightProperty().divide(rowCount))*/
+
                         root.children.add(blocked)
                     }
                     '_' -> {
-                        val free = Rectangle(TILE_WIDTH, TILE_HEIGHT)
+                        val free = Rectangle(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE)
                         free.fill = Color.LIGHTSLATEGRAY
-                        //free.stroke = Color.LIGHTSLATEGRAY
-                        free.opacity = .5
+                        /* resize x and y */
+                        //free.xProperty().bind(rectsAreaSize.multiply(x).divide(grid_x));
+
+                        /* resize width and height */
+                        /*free.widthProperty().bind(root.widthProperty().divide(columnCount))
+                        free.heightProperty().bind(root.heightProperty().divide(rowCount))*/
+
+                        free.stroke = Color.WHITE
+                        //free.opacity = .5
                         root.children.add(free)
                     }
                     '*' -> {
-                        val radius = TILE_WIDTH/10.0
-                        val dirtyCell = Circle(radius)
+                        val radius = TILE_WIDTH / 10.0
+                        val dirtyLocX = x * TILE_SIZE + ((TILE_SIZE) / 2.0)
+                        val dirtyLocY = y * TILE_SIZE + ((TILE_SIZE) / 2.0)
+                        println(dirtyLocX)
+                        println(dirtyLocY)
+
+                        val dirtyCell = Circle(dirtyLocX, dirtyLocY, radius)
                         //dirtyCell.stroke = Color.BLUE
                         dirtyCell.fill = Color.BLUE
                         root.children.add(dirtyCell)
@@ -101,55 +114,56 @@ class VaccumVisualizer : Application() {
             }
         }
 
-        primaryStage.scene = Scene(root, WIDTH + columnCount, HEIGHT + rowCount)
+        primaryStage.scene = Scene(root, TILE_SIZE * columnCount, TILE_SIZE * rowCount)
         primaryStage.show()
 
-        if(startX == null || startY == null){
+        if (startX == null || startY == null) {
             println("Start location must be defined")
             exitProcess(1)
         }
 
 
         /* Create the path that the robot will travel */
+        robot.toFront()
         val path = Path()
-        val xLoc = startX*TILE_WIDTH + ((TILE_WIDTH - robotWidth)/2.0)
-        val yLoc = startY*TILE_HEIGHT + ((TILE_HEIGHT - robotWidth)/2.0)
+        val xLoc = startX * TILE_SIZE + ((TILE_SIZE) / 2.0)
+        val yLoc = startY * TILE_SIZE + ((TILE_SIZE) / 2.0)
         robot.x = xLoc
         robot.y = yLoc
         robot.translateX = xLoc
         robot.translateY = yLoc
         path.elements.add(MoveTo(xLoc, yLoc))
-        //robot.translateX = xLoc
-        //robot.translateY = yLoc
 
-        for(action in actionList){
-            animate(action, path, robot, TILE_WIDTH, TILE_HEIGHT)
+        for (action in actionList) {
+           animate(action, path, robot, TILE_SIZE, TILE_SIZE)
         }
-        val pathTransition =  PathTransition()
-        pathTransition.setDuration(Duration.millis(10000.0))
+        val pathTransition = PathTransition()
+        pathTransition.setDuration(Duration.millis(TIME_TO_RUN))
         pathTransition.setPath(path)
         pathTransition.setNode(robot)
-        pathTransition.setAutoReverse(true)
+        pathTransition.setInterpolator(Interpolator.LINEAR);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        //pathTransition.setAutoReverse(true)
         pathTransition.play()
     }
 
-    private fun animate(action: String, path: Path, robot: Rectangle, width: Double, height: Double){
-        when(action){
+    private fun animate(action: String, path: Path, robot: Rectangle, width: Double, height: Double) {
+        when (action) {
             "UP" -> {
-                path.elements.add(LineTo(robot.translateX,robot.translateY + height + 1))
-                robot.translateY = robot.translateY + height + 1
+                path.elements.add(LineTo(robot.translateX, robot.translateY + height))
+                robot.translateY = robot.translateY + height
             }
             "RIGHT" -> {
-                path.elements.add(LineTo(robot.translateX + width +1,robot.translateY))
-                robot.translateX = robot.translateX + width + 1
+                path.elements.add(LineTo(robot.translateX + width, robot.translateY))
+                robot.translateX = robot.translateX + width
             }
             "DOWN" -> {
-                path.elements.add(LineTo(robot.translateX,robot.translateY - height -1))
-                robot.translateY = robot.translateY - height - 1
+                path.elements.add(LineTo(robot.translateX, robot.translateY - height))
+                robot.translateY = robot.translateY - height
             }
             "LEFT" -> {
-                path.elements.add(LineTo(robot.translateX - width -1,robot.translateY))
-                robot.translateX = robot.translateX - width - 1
+                path.elements.add(LineTo(robot.translateX - width, robot.translateY))
+                robot.translateX = robot.translateX - width
             }
         }
     }
