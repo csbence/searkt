823baa6edda181c5d40f33b8d15c13a792da0df4
Bence Cserna <bence@cserna.net>
2016-02-25 17:19:20 -0500

    Update ExperimentConfiguration interface to accommodate custom values

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
@@ -21,20 +21,21 @@ import java.util.*
 
 fun main(args: Array<String>) {
 
-//    val instanceFileName = "input/vacuum/dylan/uniform.vw"
-//    val rawDomain = Scanner(File(instanceFileName)).useDelimiter("\\Z").next();
-//    val manualConfiguration = ManualConfiguration("grid world", rawDomain, "LSS-LRTA*", 1, "time", 10)
-//    ConfigurationExecutor.executeConfiguration(manualConfiguration)
+    val instanceFileName = "input/vacuum/dylan/uniform.vw"
+    val rawDomain = Scanner(File(instanceFileName)).useDelimiter("\\Z").next();
+    val manualConfiguration = ManualConfiguration("grid world", rawDomain, "RTA*", 1, "time", 10)
+    manualConfiguration.setValue("lookahead depth limit", 4)
+    ConfigurationExecutor.executeConfiguration(manualConfiguration)
 
 //    val instanceFileName = "input/tiles/korf/4/87"
 //    val rawDomain = Scanner(File(instanceFileName)).useDelimiter("\\Z").next();
 //    val manualConfiguration = ManualConfiguration("sliding tile puzzle", rawDomain, "LSS-LRTA*", 1, "time", 10)
 //    ConfigurationExecutor.executeConfiguration(manualConfiguration)
 
-    val instanceFileName = "input/tiles/korf/4/98"
-    val rawDomain = Scanner(File(instanceFileName)).useDelimiter("\\Z").next();
-    val manualConfiguration = ManualConfiguration("sliding tile puzzle", rawDomain, "A*", 1, "time", 10)
-    ConfigurationExecutor.executeConfiguration(manualConfiguration)
+//    val instanceFileName = "input/tiles/korf/4/1"
+//    val rawDomain = Scanner(File(instanceFileName)).useDelimiter("\\Z").next();
+//    val manualConfiguration = ManualConfiguration("sliding tile puzzle", rawDomain, "A*", 1, "time", 10)
+//    ConfigurationExecutor.executeConfiguration(manualConfiguration)
 }
 
 fun lssLrtaStarUniformExperiment() {
@@ -71,7 +72,7 @@ fun aStartUniformExperiment(): List<ExperimentResult> {
 private fun aStarVacuumWorldExperiment(instanceFileName: String): List<ExperimentResult> {
     val vacuumWorldInstance = VacuumWorldIO.parseFromStream(FileInputStream(File(instanceFileName)))
     val aStarAgent = ClassicalAgent(ClassicalAStarPlanner(vacuumWorldInstance.domain))
-    val classicalExperiment = ClassicalExperiment<VacuumWorldState>(EmptyConfiguration, aStarAgent, vacuumWorldInstance.domain, vacuumWorldInstance.initialState)
+    val classicalExperiment = ClassicalExperiment(EmptyConfiguration, aStarAgent, vacuumWorldInstance.domain, vacuumWorldInstance.initialState)
     return classicalExperiment.run()
 }
 
