6ea42fc3632259a3c015067fccaa04fef65dc595
Mike Bogochow <mbogochow@hotmail.com>
2016-02-27 16:09:24 -0500

    Did some code cleanup and removed dependencies on link1 and link2 values which are going to be removed.
    Fixed linkLine1 and linkLine2 dimensions which would extend beyong the start and end points due to stroke.
    
    Added linkLine2 to be attached to joint2 but not yet rotating.  Changed attachment calculations to use translate since it is easier though less efficient.

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/AcrobotVisualizer.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/AcrobotVisualizer.kt
@@ -4,11 +4,8 @@ import edu.unh.cs.ai.realtimesearch.environment.acrobot.AcrobotAction
 import edu.unh.cs.ai.realtimesearch.environment.acrobot.linkLength1
 import edu.unh.cs.ai.realtimesearch.environment.acrobot.linkLength2
 import edu.unh.cs.ai.realtimesearch.logging.error
-import edu.unh.cs.ai.realtimesearch.logging.info
-import javafx.animation.KeyFrame
-import javafx.animation.KeyValue
-import javafx.animation.RotateTransition
-import javafx.animation.Timeline
+import edu.unh.cs.ai.realtimesearch.logging.trace
+import javafx.animation.*
 import javafx.application.Application
 import javafx.beans.value.WritableValue
 import javafx.event.EventHandler
@@ -19,6 +16,7 @@ import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
 import javafx.scene.shape.Line
 import javafx.scene.shape.Rectangle
+import javafx.scene.shape.StrokeLineCap
 import javafx.scene.transform.Rotate
 import javafx.scene.transform.TransformChangedEvent
 import javafx.stage.Stage
@@ -61,23 +59,28 @@ class AcrobotVisualizer : Application() {
 
         /* Graphical parameters */
         val stageBorder = 100.0
-        val linkScale = 100.0 // pixel size per meter
-        val linkWidth1 = 10.0
-        val linkWidth2 = 10.0
+        val linkScale = 175.0 // pixel size per meter
         val linkScaledLength1 = linkLength1 * linkScale
         val linkScaledLength2 = linkLength2 * linkScale
+        val linkWidth1 = linkScaledLength1 / 7.5
+        val linkWidth2 = linkWidth1
         val WIDTH = (linkScaledLength1 + linkScaledLength2) * 2 + stageBorder * 2
         val HEIGHT = WIDTH
+
+        val linkStartX1 = WIDTH / 2.0
+        val linkStartY1 = stageBorder + linkScaledLength1 + linkScaledLength2
+        val linkStartX2 = linkStartX1
+        val linkStartY2 = linkStartY1 + linkScaledLength1
 
         // Link setup
         val link1 = Rectangle(linkWidth1, linkScaledLength1)
         val link2 = Rectangle(linkWidth2, linkScaledLength2)
         link1.fill = Color.BLUE
         link2.fill = Color.BLUE
-        link1.x = WIDTH / 2.0
-        link1.y = stageBorder + linkScaledLength1 + linkScaledLength2
-        link2.x = link1.x
-        link2.y = link1.y + linkScaledLength1
+        link1.x = linkStartX1
+        link1.y = linkStartY1
+        link2.x = linkStartX2
+        link2.y = linkStartY2
 
         val linkRotate1 = Rotate()
         val linkRotate2 = Rotate()
@@ -85,59 +88,54 @@ class AcrobotVisualizer : Application() {
         linkRotate2.axis = Rotate.Z_AXIS
 //        link1.transforms.add(linkRotate1)
 //        link2.transforms.add(linkRotate2)
-        linkRotate1.pivotX = link1.x + (linkWidth1 / 2.0)
-        linkRotate1.pivotY = link1.y
-        linkRotate2.pivotX = link2.x + (linkWidth2 / 2.0)
-        linkRotate2.pivotY = link2.y
+        linkRotate1.pivotX = linkStartX1
+        linkRotate1.pivotY = linkStartY1
+        linkRotate2.pivotX = linkStartX2
+        linkRotate2.pivotY = linkStartY2
 
-        val linkLine1 = Line(linkRotate1.pivotX, linkRotate1.pivotY, linkRotate1.pivotX, linkRotate1.pivotY + linkScaledLength1)
-        val linkLine2 = Line(linkRotate2.pivotX, linkRotate2.pivotY, linkRotate2.pivotX, linkRotate2.pivotY + linkScaledLength2)
+        val linkLine1 = Line(linkStartX1, linkStartY1, linkStartX1, linkStartY1 + linkScaledLength1)
+        val linkLine2 = Line(linkStartX2, linkStartY2, linkStartX2, linkStartY2 + linkScaledLength2)
         linkLine1.strokeWidth = linkWidth1
         linkLine2.strokeWidth = linkWidth2
+        linkLine1.strokeLineCap = StrokeLineCap.BUTT
+        linkLine2.strokeLineCap = StrokeLineCap.BUTT
         linkLine1.transforms.add(linkRotate1)
         linkLine2.transforms.add(linkRotate2)
-linkLine1.onRotationStarted = EventHandler<RotateEvent> { logger.info { "linkLine1 rotation started" } }
-linkLine1.onRotate = EventHandler<RotateEvent> { logger.info { "linkLine1 rotation" } }
-linkLine1.onRotationFinished = EventHandler<RotateEvent> { logger.info { "linkLine1 rotation finished" } }
-logger.info { "Line1: $linkLine1" }
-logger.info { "translate1: (${linkLine1.translateX}, ${linkLine1.translateY})" }
-logger.info { "Rotate1: $linkRotate1" }
+logger.trace { "Line1: $linkLine1" }
+logger.trace { "translate1: (${linkLine1.translateX}, ${linkLine1.translateY})" }
+logger.trace { "Rotate1: $linkRotate1" }
 //        linkRotate1.angle = 90.0
 //        linkRotate2.angle = 90.0
 
         // Joint setup
-        val joint1 = Circle(linkLine1.startX, linkLine1.startY, linkWidth1 / 2 + 1, Color.RED)
-        val joint2 = Circle(linkLine1.endX, linkLine1.endY, linkWidth2 / 2 + 1, joint1.fill)
+        val joint1 = Circle(linkLine1.startX, linkLine1.startY, linkWidth1 * 0.6, Color.RED)
+        val joint2 = Circle(linkLine1.endX, linkLine1.endY, joint1.radius, joint1.fill)
 
-linkRotate1.onTransformChanged = EventHandler<TransformChangedEvent> {
-    logger.info { "linkRotate1 transform changed: mxx: ${linkRotate1.mxx}, mxy: ${linkRotate1.mxy}, myx: ${linkRotate1.myx}, myy: ${linkRotate1.myy}" }
-//    logger.info { "angle: ${Math.atan2(-linkRotate1.myx, linkRotate1.mxx)}" }
-//    logger.info { "angle: ${Math.atan2(-linkRotate1.mxy, linkRotate1.mxx)}" }
-//    logger.info { "angle: ${Math.atan2(linkRotate1.myx, linkRotate1.myy)}" }
-    // start (400,305)
-//    val newX = joint2.centerX * linkRotate1.mxx + joint2.centerY * linkRotate1.mxy
-//    val newY = joint2.centerX * linkRotate1.myx + joint2.centerY * linkRotate1.myy
-    var angle = Math.atan2(-linkRotate1.mxy, linkRotate1.mxx) + Math.PI / 2
-    angle = if (angle < 0) angle + 2 * Math.PI else if (angle > 2 * Math.PI) angle - 2 * Math.PI else angle
-    logger.info { "angle: $angle" }
-    val newX = linkScaledLength1 * Math.cos(angle)
-    val newY = linkScaledLength1 * Math.sin(angle)
-    joint2.centerX = newX + linkLine1.startX
-    joint2.centerY = newY + linkLine1.startY
-    logger.info { "Join2: $joint2" }
-//    val newX2 = linkScaledLength1 * linkRotate1.mxx
-//    val newY2 = linkScaledLength1 * linkRotate1.myx
-//    joint2.centerX = newX2
-//    joint2.centerY = newY2
-//    logger.info { "Join2: $joint2" }
-}
+        linkRotate1.onTransformChanged = EventHandler<TransformChangedEvent> {
+        //    logger.trace { "linkRotate1 transform changed: mxx: ${linkRotate1.mxx}, mxy: ${linkRotate1.mxy}, myx: ${linkRotate1.myx}, myy: ${linkRotate1.myy}" }
+            var angle = Math.atan2(-linkRotate1.mxy, linkRotate1.mxx) + Math.PI / 2
+            angle = if (angle < 0) angle + 2 * Math.PI else if (angle > 2 * Math.PI) angle - 2 * Math.PI else angle
+        //    logger.trace { "angle: $angle" }
+            val newX = linkLine1.startX + linkScaledLength1 * Math.cos(angle)
+            val newY = linkLine1.startY + linkScaledLength1 * Math.sin(angle)
+//            joint2.centerX = newX
+//            joint2.centerY = newY
+            // Translate works best but the CPU and Memory performance grows over time without node caching
+            val translateX = newX - joint2.centerX
+            val translateY = newY - joint2.centerY
+            joint2.translateX = translateX
+            joint2.translateY = translateY
+            //    logger.trace { "Join2: $joint2" }
+            linkLine2.translateX = translateX
+            linkLine2.translateY = translateY
+//            linkLine2.startX = newX
+//            linkLine2.startY = newY
+        }
 
         root.children.add(link1)
         root.children.add(link2)
         root.children.add(linkLine1)
-//        root.children.add(linkLine2)
-//        root.children.add(link1_2)
-//        root.children.add(link2_2)
+        root.children.add(linkLine2)
         root.children.add(joint1)
         root.children.add(joint2)
 
@@ -145,22 +143,14 @@ linkRotate1.onTransformChanged = EventHandler<TransformChangedEvent> {
         primaryStage.show()
 
         /* Animate the links */
-        /*
-        RotateTransition does not allow rotating around a pivot
-         */
-//        val rotateTransition = RotateTransition()
-//        rotateTransition.duration = Duration.millis(1000.0)
-//        rotateTransition.node = link1
-//        rotateTransition.byAngle = 90.0
-//        rotateTransition.play()
-logger.info { "Join2: $joint2" }
         val timeline: Timeline = Timeline(
                 KeyFrame(Duration.ZERO, KeyValue(linkRotate1.angleProperty() as WritableValue<Any>, linkRotate1.angle)),
-                KeyFrame(Duration.seconds(10.0), KeyValue(linkRotate1.angleProperty() as WritableValue<Any>, linkRotate1.angle - 360.0)));
+                KeyFrame(Duration.seconds(1.0), KeyValue(linkRotate1.angleProperty() as WritableValue<Any>, linkRotate1.angle - 360.0)));
+//        timeline.cycleCount = Animation.INDEFINITE
         timeline.play()
 
-logger.info { "Line1: $linkLine1" }
-logger.info { "translate1: (${linkLine1.translateX}, ${linkLine1.translateY})" }
-logger.info { "Rotate1: $linkRotate1" }
+logger.trace { "Line1: $linkLine1" }
+logger.trace { "translate1: (${linkLine1.translateX}, ${linkLine1.translateY})" }
+logger.trace { "Rotate1: $linkRotate1" }
     }
 }
