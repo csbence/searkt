6a08bdf58e9ba243711f03a088f39a3ec19bcbf1
Bence Cserna <bence@cserna.net>
2016-03-04 16:40:28 -0500

    Clean up main

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
@@ -38,73 +38,77 @@ fun main(args: Array<String>) {
 
     } else {
         /* create options */
-        val options = Options()
+        createCommandLineMenu(args)
+    }
+}
 
-        options.addOption("h", "help", false, "Print help and exit")
-        options.addOption("d", "domain", true, "The domain name")
-        options.addOption("m", "map", true, "The path to map file")
-        options.addOption("a", "alg-name", true, "The algorithm name")
-        options.addOption("n", "num-runs", true, "The number of runs")
-        options.addOption("t", "term-type", true, "The termination type")
-        options.addOption("p", "term-param", true, "The termination parameter")
-        options.addOption("v", "visualize", false, "Whether or not to visualize")
-        options.addOption("o", "outfile", true, "Outfile of experiments")
+private fun createCommandLineMenu(args: Array<String>) {
+    val options = Options()
+
+    options.addOption("h", "help", false, "Print help and exit")
+    options.addOption("d", "domain", true, "The domain name")
+    options.addOption("m", "map", true, "The path to map file")
+    options.addOption("a", "alg-name", true, "The algorithm name")
+    options.addOption("n", "num-runs", true, "The number of runs")
+    options.addOption("t", "term-type", true, "The termination type")
+    options.addOption("p", "term-param", true, "The termination parameter")
+    options.addOption("v", "visualize", false, "Whether or not to visualize")
+    options.addOption("o", "outfile", true, "Outfile of experiments")
 
 
-        /* parse command line arguments */
-        val parser = GnuParser()
-        val cmd = parser.parse(options, args)
-        val domainName = cmd.getOptionValue('d')
-        val mapFile = cmd.getOptionValue('m')
-        val algName = cmd.getOptionValue('a')
-        val numRuns = cmd.getOptionValue('n')
-        val termType = cmd.getOptionValue('n')
-        val termParam = cmd.getOptionValue('n')
-        val outFile = cmd.getOptionValue('o')
+    /* parse command line arguments */
+    val parser = GnuParser()
+    val cmd = parser.parse(options, args)
+    val domainName = cmd.getOptionValue('d')
+    val mapFile = cmd.getOptionValue('m')
+    val algName = cmd.getOptionValue('a')
+    val numRuns = cmd.getOptionValue('n')
+    val termType = cmd.getOptionValue('n')
+    val termParam = cmd.getOptionValue('n')
+    val outFile = cmd.getOptionValue('o')
 
-        /* print help if help option was specified*/
-        val formatter = HelpFormatter()
-        if (cmd.hasOption("h")) {
-            formatter.printHelp("real-time-search", options)
-            exitProcess(1)
-        }
+    /* print help if help option was specified*/
+    val formatter = HelpFormatter()
+    if (cmd.hasOption("h")) {
+        formatter.printHelp("real-time-search", options)
+        exitProcess(1)
+    }
 
-        /* print help if any options weren't specified */
-        if (domainName == null || mapFile == null || numRuns == null ||
-                termType == null || termParam == null || outFile == null) {
-            formatter.printHelp("real-time-search", options)
-            exitProcess(1)
-        }
+    /* print help if any options weren't specified */
+    if (domainName == null || mapFile == null || numRuns == null ||
+            termType == null || termParam == null || outFile == null) {
+        formatter.printHelp("real-time-search", options)
+        exitProcess(1)
+    }
 
-        /* run the experiment */
-        val rawDomain = Scanner(File(mapFile)).useDelimiter("\\Z").next();
-        val manualConfiguration = ManualConfiguration(domainName, rawDomain, algName,
-                numRuns.toInt(), termType, termParam.toInt())
-        val resultList = ConfigurationExecutor.executeConfiguration(manualConfiguration)
+    /* run the experiment */
+    val rawDomain = Scanner(File(mapFile)).useDelimiter("\\Z").next();
+    val manualConfiguration = ManualConfiguration(domainName, rawDomain, algName,
+            numRuns.toInt(), termType, termParam.toInt())
+    val resultList = ConfigurationExecutor.executeConfiguration(manualConfiguration)
 
-        /* output the results */
-        val writer = PrintWriter(outFile, "UTF-8");
-        val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-        val date = Date()
+    /* output the results */
+    val writer = PrintWriter(outFile, "UTF-8");
+    val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+    val date = Date()
 
-        for (result in resultList) {
-            writer.println("Date: " + dateFormat.format(date))
-            writer.println("Hostname: " + InetAddress.getLocalHost().getHostName())
-            writer.println("Termination Type: " + termType)
-            writer.println("Termination parameter: " + termParam)
-            writer.println("Expanded nodes: " + result.expandedNodes)
-            writer.println("Generated nodes: " + result.generatedNodes)
-            writer.println("Time in millis: " + result.timeInMillis)
-            writer.println("Action list: " + result.actions)
-            writer.println("Path length: " + result.pathLength)
-        }
-        writer.close()
+    for (result in resultList) {
+        writer.println("Date: " + dateFormat.format(date))
+        writer.println("Hostname: " + InetAddress.getLocalHost().getHostName())
+        writer.println("Termination Type: " + termType)
+        writer.println("Termination parameter: " + termParam)
+        writer.println("Expanded nodes: " + result.expandedNodes)
+        writer.println("Generated nodes: " + result.generatedNodes)
+        writer.println("Time in millis: " + result.timeInMillis)
+        writer.println("Action list: " + result.actions)
+        writer.println("Path length: " + result.pathLength)
+    }
+    writer.close()
 
-        if (options.hasOption("v")) {
-            /* visualize the output */
-            /* TODO: Make visualizer easier to use and read from file, then merge with master
+    if (options.hasOption("v")) {
+        /* visualize the output */
+        /* TODO: Make visualizer easier to use and read from file, then merge with master
              */
 
-        }
     }
 }
