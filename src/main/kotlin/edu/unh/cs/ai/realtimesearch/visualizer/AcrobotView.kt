1a338db1c5417a6943c4010d9b8bcaf96b9a5f9b
Mike Bogochow <mbogochow@hotmail.com>
2016-03-04 16:58:59 -0500

    Added support for ghost visualization which shows the next state that the acrobot is going to move to.
    Changed animation calculate states from the action list prior to running the animation.  Setting up the animation now reads states instead of actions.  This allows for only calculating them once for multiple animations.
    
    Changed VisualAcrobot to AcrobotView and added properties.

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/AcrobotView.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/AcrobotView.kt
@@ -5,13 +5,14 @@ import edu.unh.cs.ai.realtimesearch.environment.acrobot.linkLength2
 import javafx.event.EventHandler
 import javafx.scene.Node
 import javafx.scene.paint.Color
+import javafx.scene.paint.Paint
 import javafx.scene.shape.Circle
 import javafx.scene.shape.Line
 import javafx.scene.shape.StrokeLineCap
 import javafx.scene.transform.Rotate
 import javafx.scene.transform.TransformChangedEvent
 
-data class VisualAcrobot(val linkStartX1: Double, val linkStartY1: Double, val linkScale: Double, val linkWidth: Double) {
+data class AcrobotView(val linkStartX1: Double, val linkStartY1: Double, val linkScale: Double, val linkWidth: Double) {
     val linkScaledLength1 = linkLength1 * linkScale
     val linkScaledLength2 = linkLength2 * linkScale
     val linkStartX2 = linkStartX1
@@ -22,14 +23,57 @@ data class VisualAcrobot(val linkStartX1: Double, val linkStartY1: Double, val l
     val link2 = Line(linkStartX2, linkStartY2, linkStartX2, linkStartY2 + linkScaledLength2)
 
     // Joint setup
-    val joint1 = Circle(linkStartX1, linkStartY1, linkWidth * 0.6, Color.RED)
-    val joint2 = Circle(linkStartX2, linkStartY2, joint1.radius, joint1.fill)
+    val joint1 = Circle(linkStartX1, linkStartY1, linkWidth * 0.6)
+    val joint2 = Circle(linkStartX2, linkStartY2, joint1.radius)
+
 
     // Rotation setup
     val linkRotate1 = Rotate(0.0, linkStartX1, linkStartY1, 0.0, Rotate.Z_AXIS)
     val linkRotate2 = Rotate(0.0, linkStartX2, linkStartY2, 0.0, Rotate.Z_AXIS)
 
+    var opacity: Double
+        get() = link1.opacity
+        set(value) {
+            link1.opacity = value
+            link2.opacity = value
+            joint1.opacity = value
+            joint2.opacity = value
+        }
+
+    var isVisible: Boolean
+        get() = link1.isVisible
+        set(value) {
+            link1.isVisible = value
+            link2.isVisible = value
+            joint1.isVisible = value
+            joint2.isVisible = value
+        }
+
+    var linkColor: Paint
+        get() = link1.stroke
+        set(value) {
+            link1.stroke = value
+            link2.stroke = value
+        }
+
+    var jointColor: Paint
+        get() = joint1.fill
+        set(value) {
+            joint1.fill = value
+            joint2.fill = value
+        }
+
+    fun toBack() {
+        link1.toBack()
+        link2.toBack()
+        joint1.toBack()
+        joint2.toBack()
+    }
+
     init {
+        jointColor = Color.RED
+        linkColor = Color.BLACK
+
         link1.strokeWidth = linkWidth
         link2.strokeWidth = linkWidth
         link1.strokeLineCap = StrokeLineCap.BUTT
--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/AcrobotVisualizer.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/AcrobotVisualizer.kt
@@ -3,11 +3,14 @@ package edu.unh.cs.ai.realtimesearch.visualizer
 import edu.unh.cs.ai.realtimesearch.environment.acrobot.*
 import edu.unh.cs.ai.realtimesearch.logging.debug
 import edu.unh.cs.ai.realtimesearch.logging.error
+import edu.unh.cs.ai.realtimesearch.logging.info
 import javafx.animation.*
 import javafx.application.Application
 import javafx.beans.value.WritableValue
+import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
 import javafx.stage.Stage
 import javafx.util.Duration
 import org.slf4j.LoggerFactory
@@ -26,10 +29,17 @@ open class AcrobotVisualizer : Application() {
 //            exitProcess(1)
 //        }
 //        val rawDomain = raw.first()
+        var ghost: Boolean = false
+        var start = 0
+
+        if (raw[0].equals("-g")) {
+            ghost = true
+            start += 1
+        }
 
         /* Get action list from Application */
         val actionList: MutableList<AcrobotAction> = mutableListOf()
-        for (i in 1..raw.size - 1) {
+        for (i in start..raw.lastIndex) {
             val action: String = raw[i]
             try {
                 actionList.add(AcrobotAction.valueOf(action))
@@ -76,6 +86,9 @@ open class AcrobotVisualizer : Application() {
         actionList.add(AcrobotAction.NEGATIVE)
         actionList.add(AcrobotAction.NONE)
 
+        val stateList = getStateList(actionList)
+        assert(stateList.size > 1, {"Must have at least two states to animate"})
+
         /* Graphical parameters */
         val stageBorder = 100.0
         val linkScale = 175.0 // pixel size per meter
@@ -88,48 +101,70 @@ open class AcrobotVisualizer : Application() {
         val linkStartX1 = WIDTH / 2.0
         val linkStartY1 = stageBorder + linkScaledLength1 + linkScaledLength2
 
-        val visualAcrobot = VisualAcrobot(linkStartX1, linkStartY1, linkScaledLength1, linkWidth)
+        val acrobotView = AcrobotView(linkStartX1, linkStartY1, linkScaledLength1, linkWidth)
 
         // Add everything to the stage
         val rootPane = Pane()
-        rootPane.children.addAll(visualAcrobot.getNodes())
+        rootPane.children.addAll(acrobotView.getNodes())
 
         primaryStage.scene = Scene(rootPane, WIDTH, HEIGHT)
         primaryStage.show()
 
-        animateAcrobot(visualAcrobot, actionList)
+        animateAcrobot(acrobotView, stateList)
+        
+        // Animate a ghost acrobot if desired
+        if (ghost) {
+            val ghostAcrobot = AcrobotView(linkStartX1, linkStartY1, linkScaledLength1, linkWidth)
+            ghostAcrobot.opacity = 0.5
+            ghostAcrobot.linkColor = Color.GRAY
+
+            rootPane.children.addAll(ghostAcrobot.getNodes())
+            ghostAcrobot.toBack()
+
+            val ghostTransition = animateAcrobot(ghostAcrobot, stateList.subList(1, stateList.size), Interpolator.DISCRETE)
+            ghostTransition.onFinished = EventHandler {
+                ghostAcrobot.isVisible = false
+            }
+        }
     }
 
-    protected open fun animateAcrobot(visualAcrobot: VisualAcrobot, actionList: List<AcrobotAction>,
-                                      getInterpolator1: (state: AcrobotState, action: AcrobotAction) -> Interpolator = { state, action -> Interpolator.LINEAR},
-                                      getInterpolator2: (state: AcrobotState, action: AcrobotAction) -> Interpolator = { state, action -> getInterpolator1(state, action)}): Animation {
-        /* Animate the links */
+    private fun getStateList(actionList: List<AcrobotAction>): List<AcrobotState> {
+        val stateList = mutableListOf<AcrobotState>()
         // TODO setup links according to initial state values
         val acrobot = DiscretizedAcrobot()
         val environment = DiscretizedAcrobotEnvironment(acrobot) // TODO read optional initial state from input
-        val sequentialTransition = SequentialTransition()
-        @Suppress("UNCHECKED_CAST")
-        sequentialTransition.children.add(Timeline(60.0, KeyFrame(Duration.ZERO,
-                KeyValue(visualAcrobot.linkRotate1.angleProperty() as WritableValue<Any>, visualAcrobot.linkRotate1.angle),
-                KeyValue(visualAcrobot.linkRotate2.angleProperty() as WritableValue<Any>, visualAcrobot.linkRotate2.angle))))
-
-        var previousState = environment.getState().state
-        val time = timeStep
         for (action in actionList) {
             environment.step(action)
-            val newState = environment.getState().state
+            stateList.add(environment.getState().state)
+        }
+        return stateList
+    }
 
+    protected open fun animateAcrobot(acrobotView: AcrobotView, stateList: List<AcrobotState>,
+                                      interpolator1: Interpolator = Interpolator.EASE_IN,
+                                      interpolator2: Interpolator = interpolator1): Animation {
+        /* Animate the links */
+        val sequentialTransition = SequentialTransition()
+
+        if (stateList.size < 1)
+            throw IllegalArgumentException("State list must have at least one state for animation")
+
+        val iterator = stateList.iterator()
+        var previousState = iterator.next()
+        val time = timeStep
+        while (iterator.hasNext()) {
+            val newState = iterator.next()
             val diff1 = Math.toDegrees(angleDifference(previousState.linkPosition1, newState.linkPosition1))
             val diff2 = Math.toDegrees(angleDifference(previousState.linkPosition2, newState.linkPosition2)) + diff1
 
-            val newRotate1 = visualAcrobot.addRotate1()
-            val newRotate2 = visualAcrobot.addRotate2()
+            val newRotate1 = acrobotView.addRotate1()
+            val newRotate2 = acrobotView.addRotate2()
 
             logger.debug { "Adding (${String.format("%.1f", time)}: $diff1, $diff2) to timeline" }
             @Suppress("UNCHECKED_CAST")
             sequentialTransition.children.add(Timeline(60.0, KeyFrame(Duration.seconds(time),
-                    KeyValue(newRotate1.angleProperty() as WritableValue<Any>, -diff1, getInterpolator1(previousState, action)),
-                    KeyValue(newRotate2.angleProperty() as WritableValue<Any>, -diff2, getInterpolator2(previousState, action)))))
+                    KeyValue(newRotate1.angleProperty() as WritableValue<Any>, -diff1, interpolator1),
+                    KeyValue(newRotate2.angleProperty() as WritableValue<Any>, -diff2, interpolator2))))
 
             previousState = newState
         }
