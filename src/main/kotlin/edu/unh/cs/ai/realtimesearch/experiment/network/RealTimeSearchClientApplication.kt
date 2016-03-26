f3abc535ecdb89bf5c7654b9b4840f3241cf016c
Bence Cserna <bence@cserna.net>
2016-03-26 17:01:44 -0400

    Fix client application

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/experiment/network/RealTimeSearchClientApplication.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/experiment/network/RealTimeSearchClientApplication.kt
@@ -1,19 +1,20 @@
 package edu.unh.cs.ai.realtimesearch.experiment.network
 
 import edu.unh.cs.ai.realtimesearch.experiment.configuration.ConfigurationExecutor
+import org.apache.commons.cli.*
 import java.util.*
 import kotlin.concurrent.timerTask
+
 
 /**
  * @author Bence Cserna (bence@cserna.net)
  */
-class RealTimeSearchClientApplication(private val rtsServerUrl: String, private val checkInInterval: Long = 10000) {
+class RealTimeSearchClientApplication(private val rtsServerUrl: String, private val checkInInterval: Long = 60000) {
 
     private val timer: Timer = Timer()
     private val realTimeSearchClient: RealTimeSearchClient
     private var checkInTask: TimerTask? = null
     private @Volatile var running = false
-
 
     init {
         if (checkInInterval !in 0..3600000) {
@@ -40,18 +41,18 @@ class RealTimeSearchClientApplication(private val rtsServerUrl: String, private 
             // Get configuration
             val experimentConfiguration = realTimeSearchClient.getExperimentConfiguration()
             if (experimentConfiguration != null) {
-                System.gc()
+                stopPeriodicCheckIn() // Don't do anything else parallel to the experiment
+                System.gc() // Make sure that we have not garbage in the memory
 
                 // Execute configuration
                 val experimentResult = ConfigurationExecutor.executeConfiguration(experimentConfiguration)
 
                 // Submit results
                 realTimeSearchClient.submitResult(experimentResult)
-
-                // TODO handle errors gracefully
+                startPeriodicCheckIn()
             } else {
                 // Failed to get a a configuration wait a second to avoid busy wait
-                Thread.sleep(1000)
+                Thread.sleep(10000)
             }
         }
     }
@@ -75,5 +76,26 @@ class RealTimeSearchClientApplication(private val rtsServerUrl: String, private 
             checkInTask = null
             timer.purge()
         }
+    }
+}
+
+fun main(args: Array<String>) {
+    val mainOptions = Options()
+
+    val serverUrlOption = Option("u", "server-url", true, "RTS server address")
+    val helpOption = Option("h", "help", false, "Print this help and exit")
+
+    mainOptions.addOption(serverUrlOption)
+
+    val parser: CommandLineParser = DefaultParser();
+
+    val commandLine = parser.parse(mainOptions, args)
+
+    val formatter = HelpFormatter()
+    if (commandLine.hasOption(helpOption.opt) || !commandLine.hasOption(serverUrlOption.opt)) {
+        formatter.printHelp("Real-time Search Client", mainOptions)
+        return
+    } else {
+        RealTimeSearchClientApplication(commandLine.getOptionValue(serverUrlOption.opt)).start()
     }
 }
