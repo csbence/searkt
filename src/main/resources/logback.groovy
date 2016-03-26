cb54d34da3762a8a324e295ded4ccc3bd28bd777
Bence Cserna <bence@cserna.net>
2016-03-26 17:19:54 -0400

    Fix log hostname

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -4,6 +4,7 @@ import ch.qos.logback.core.FileAppender
 
 // define the USER_HOME variable setting its value
 // to that of the "user.home" system property
+def HOSTNAME=hostname
 def USER_HOME = System.getProperty("user.home")
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
@@ -14,7 +15,7 @@ appender("STDOUT", ConsoleAppender) {
 appender("FILE", FileAppender) {
     // make use of the USER_HOME variable
     println "Setting [file] property to [/home/aifs2/group/data/real_time_search_log/rts_client_${hostname}.log]"
-    file =  "/home/aifs2/group/data/real_time_search_log/rts_client_${hostname}.log"
+    file =  "/home/aifs2/group/data/real_time_search_log/rts_client_${HOSTNAME}.log"
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} %5level %logger{0} - %msg%n"
     }
