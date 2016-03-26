6cd54a8f364313278e9c3be33990d9e187bd5691
Bence Cserna <bence@cserna.net>
2016-03-25 20:54:02 -0400

    Add file appender to logback

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,24 +1,24 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
 
 // define the USER_HOME variable setting its value
 // to that of the "user.home" system property
 def USER_HOME = System.getProperty("user.home")
-
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} %5level %logger{0} - %msg%n"
     }
 }
 
-//appender("FILE", FileAppender) {
-//    // make use of the USER_HOME variable
-//    println "Setting [file] property to [${USER_HOME}/RTS.log]"
-//    file =  "${USER_HOME}/RTS.log"
-//    encoder(PatternLayoutEncoder) {
-//        pattern = "%msg%n"
-//    }
-//}
+appender("FILE", FileAppender) {
+    // make use of the USER_HOME variable
+    println "Setting [file] property to [/home/aifs2/group/data/real_time_search_log/rts_client_${hostname}.log]"
+    file =  "/home/aifs2/group/data/real_time_search_log/rts_client_${hostname}.log"
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS} %5level %logger{0} - %msg%n"
+    }
+}
 
 root(INFO, ["STDOUT"])
 
