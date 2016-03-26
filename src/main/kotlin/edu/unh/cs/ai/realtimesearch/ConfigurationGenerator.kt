c78b4222ae6ad38ecba68a535ecba12736b9cb32
Bence Cserna <bence@cserna.net>
2016-03-26 16:31:05 -0400

    Fix configuration generator

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/ConfigurationGenerator.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/ConfigurationGenerator.kt
@@ -2,10 +2,13 @@ package edu.unh.cs.ai.realtimesearch
 
 import edu.unh.cs.ai.realtimesearch.environment.Domains
 import edu.unh.cs.ai.realtimesearch.environment.Domains.*
-import edu.unh.cs.ai.realtimesearch.experiment.configuration.ExperimentData
+import edu.unh.cs.ai.realtimesearch.experiment.configuration.realtime.TimeBoundType
+import edu.unh.cs.ai.realtimesearch.experiment.configuration.realtime.TimeBoundType.DYNAMIC
+import edu.unh.cs.ai.realtimesearch.experiment.configuration.realtime.TimeBoundType.STATIC
+import edu.unh.cs.ai.realtimesearch.planner.CommitmentStrategy
+import edu.unh.cs.ai.realtimesearch.planner.CommitmentStrategy.SINGLE
 import edu.unh.cs.ai.realtimesearch.planner.Planners
 import edu.unh.cs.ai.realtimesearch.planner.Planners.*
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -15,10 +18,9 @@ import java.util.concurrent.TimeUnit
 
 val terminationType = "time"
 val timeLimit = TimeUnit.NANOSECONDS.convert(300, TimeUnit.SECONDS)
-val timeBoundTypes = listOf("STATIC", "DYNAMIC")
 val actionDurationRange = 1..5
-val actionDurations = actionDurationRange.map { Math.pow(10.0, it.toDouble()) * 100 }
-val lookaheadLimits = 1..10
+val actionDurations = actionDurationRange.map { Math.pow(10.0, it.toDouble()).toLong() * 100 }
+val lookaheadLimits = 1..6
 
 fun main(args: Array<String>) {
     val configurations = mutableListOf<MutableMap<String, Any?>>()
@@ -26,7 +28,13 @@ fun main(args: Array<String>) {
     for (domain in Domains.values()) {
         for (planner in Planners.values()) {
             for (actionDuration in actionDurations) {
-                val configuration = mutableMapOf<String, Any?>(
+
+                // TODO integrate acrobot configurations
+                if (domain == ACROBOT) {
+                    continue
+                }
+
+                val partialConfiguration = mutableMapOf<String, Any?>(
                         "domainName" to domain,
                         "algorithmName" to planner,
                         "actionDuration" to actionDuration,
@@ -35,25 +43,31 @@ fun main(args: Array<String>) {
                         "terminationType" to terminationType
                 )
 
-                val realTimePlannerConfigurations = realTimePlanners(planner)
+                val realTimePlannerConfigurations = getRealTimePlannerConfigurations(planner)
+                val domainConfigurations = getDomainConfigurations(domain)
+
                 for (realTimePlannerConfiguration in realTimePlannerConfigurations) {
-                    val domainConfigurations = getDomainConfigurations(domain)
                     for (domainConfiguration in domainConfigurations) {
-                        realTimePlannerConfiguration.putAll(domainConfiguration)
+
+                        val completeConfiguration = mutableMapOf<String, Any?>()
+
+                        completeConfiguration.putAll(partialConfiguration)
+                        completeConfiguration.putAll(realTimePlannerConfiguration)
+                        completeConfiguration.putAll(domainConfiguration)
+
+                        configurations.add(completeConfiguration)
                     }
-                    realTimePlannerConfiguration.putAll(configuration)
+
                 }
-                // TODO integrate acrobot configurations
-                if (!domain.equals(ACROBOT))
-                    configurations.addAll(realTimePlannerConfigurations)
             }
         }
     }
 
-//    for (configuration in configurations) {
-//        println(configuration)
-//    }
+    //    for (configuration in configurations) {
+    //        println(configuration)
+    //    }
 
+    println("${configurations.size} configuration were generated.")
     uploadConfigurations(configurations)
 }
 
@@ -80,7 +94,12 @@ fun getDomainConfigurations(domain: Domains): MutableList<MutableMap<String, Any
             "input/pointrobot/wall2.pr"
     )
 
-    val slidingTileMaps = "input/tiles/korf/4/"
+    val slidingTile4MapRoot = "input/tiles/korf/4/all/"
+    val slidingTile25EasyMapRoot = "input/tiles/korf/4/25_easy/"
+    val slidingTileEasyMapRoot = "input/tiles/korf/4/easy"
+
+    val slidingTile25EasyMapNames = listOf(9, 12, 19, 28, 30, 31, 42, 45, 47, 48, 55, 57, 61, 71, 73, 74, 79, 81, 85, 86, 90, 93, 94, 95, 97)
+    val slidingTIleEasyMapNames = listOf (5, 6, 8, 9, 12, 13, 19, 20, 23, 28, 30, 31, 34, 38, 39, 42, 45, 46, 47, 48, 51, 55, 57, 58, 61, 62, 65, 71, 73, 74, 77, 78, 79, 81, 85, 86, 90, 93, 94, 95, 96, 97)
 
     when (domain) {
         ACROBOT -> {
@@ -90,7 +109,8 @@ fun getDomainConfigurations(domain: Domains): MutableList<MutableMap<String, Any
             for (map in gridMaps) {
                 val input = GRID_WORLD.javaClass.classLoader.getResourceAsStream(map)
                 configurations.add(mutableMapOf(
-                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next()
+                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next(),
+                        "domainInstanceName" to map
                 ))
             }
         }
@@ -98,15 +118,18 @@ fun getDomainConfigurations(domain: Domains): MutableList<MutableMap<String, Any
             for (map in racetrackMaps) {
                 val input = GRID_WORLD.javaClass.classLoader.getResourceAsStream(map)
                 configurations.add(mutableMapOf(
-                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next()
+                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next(),
+                        "domainInstanceName" to map
                 ))
             }
         }
         SLIDING_TILE_PUZZLE -> {
-            for (i in 1..100) {
-                val input = GRID_WORLD.javaClass.classLoader.getResourceAsStream("$slidingTileMaps$i")
+            for (instanceName in slidingTile25EasyMapNames) {
+                val map = "$slidingTile4MapRoot$instanceName"
+                val input = GRID_WORLD.javaClass.classLoader.getResourceAsStream(map)
                 configurations.add(mutableMapOf(
-                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next()
+                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next(),
+                        "domainInstanceName" to map
                 ))
             }
         }
@@ -114,7 +137,8 @@ fun getDomainConfigurations(domain: Domains): MutableList<MutableMap<String, Any
             for (map in pointRobotMaps) {
                 val input = GRID_WORLD.javaClass.classLoader.getResourceAsStream(map)
                 configurations.add(mutableMapOf(
-                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next()
+                        "rawDomain" to Scanner(input).useDelimiter("\\Z").next(),
+                        "domainInstanceName" to map
                 ))
             }
         }
@@ -123,24 +147,44 @@ fun getDomainConfigurations(domain: Domains): MutableList<MutableMap<String, Any
     return configurations
 }
 
-fun realTimePlanners(planner: Planners): MutableList<MutableMap<String, Any?>> {
+fun getRealTimePlannerConfigurations(planner: Planners): MutableList<MutableMap<String, Any?>> {
     val configurations = mutableListOf<MutableMap<String, Any?>>()
 
     when (planner) {
         DYNAMIC_F_HAT, LSS_LRTA_STAR -> {
-            for (timeBoundType in timeBoundTypes) {
-                configurations.add(mutableMapOf<String, Any?>(
-                        "timeBoundType" to timeBoundType
-                ))
+            for (timeBoundType in TimeBoundType.values()) {
+
+                when (timeBoundType) {
+                    STATIC -> {
+                        configurations.add(mutableMapOf<String, Any?>(
+                                "timeBoundType" to timeBoundType,
+                                "commitmentStrategy" to SINGLE
+                        ))
+                        configurations.add(mutableMapOf<String, Any?>(
+                                "timeBoundType" to timeBoundType,
+                                "commitmentStrategy" to CommitmentStrategy.MULTIPLE
+                        ))
+                    }
+                    DYNAMIC -> {
+                        configurations.add(mutableMapOf<String, Any?>(
+                                "timeBoundType" to timeBoundType,
+                                "commitmentStrategy" to CommitmentStrategy.MULTIPLE
+                        ))
+                    }
+                }
+
             }
         }
         RTA_STAR -> {
             for (lookaheadDepthLimit in lookaheadLimits) {
                 configurations.add(mutableMapOf<String, Any?>(
-                        "lookaheadDepthLimit" to lookaheadDepthLimit
+                        "lookaheadDepthLimit" to lookaheadDepthLimit,
+                        "timeBoundType" to STATIC,
+                        "commitmentStrategy" to SINGLE
                 ))
             }
         }
+        else -> configurations.add(mutableMapOf())
     }
 
     return configurations
@@ -148,20 +192,15 @@ fun realTimePlanners(planner: Planners): MutableList<MutableMap<String, Any?>> {
 
 fun uploadConfigurations(configurations: MutableList<MutableMap<String, Any?>>) {
     val restTemplate = RestTemplate()
-    val serverUrl = "http://aerials.cs.unh.edu:3824"
-    var successCounter = 0
-    var failCounter = 0
+    //    val serverUrl = "http://aerials.cs.unh.edu:3824
+    var serverUrl = "http://localhost:3824/configurations"
 
-    for (configuration in configurations) {
-        val configurationData = ExperimentData(configuration)
-
-        val responseEntity = restTemplate.exchange(serverUrl, HttpMethod.POST, HttpEntity(configurationData), Nothing::class.java)
-        if (responseEntity.statusCode == HttpStatus.OK) {
-            successCounter++
-        } else {
-            failCounter++
-        }
+    println("Upload generated files. ${configurations.size}")
+    val responseEntity = restTemplate.exchange(serverUrl, HttpMethod.POST, HttpEntity(configurations), Nothing::class.java)
+    if (responseEntity.statusCode == HttpStatus.OK) {
+        println("Upload completed! ${configurations.size}")
+    } else {
+        println("Upload failed! ${configurations.size}")
     }
 
-    println("Upload completed! Successfully uploaded: $successCounter Failed to upload: $failCounter")
 }
