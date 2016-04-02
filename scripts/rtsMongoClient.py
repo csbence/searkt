6fe6aa263caff28d058dfa18285e2d9904d7ca42
Mike Bogochow <mbogochow@hotmail.com>
2016-04-02 10:51:33 -0400

    Added command line options.

--- scripts/rtsMongoClient.py
+++ scripts/rtsMongoClient.py
@@ -1,10 +1,36 @@
 #!/usr/bin/python3
 
+import getopt
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import sys
+from enum import Enum
 from pymongo import MongoClient
 
 import plotutils
+
+
+class GraphType(Enum):
+    gatPerDuration = 1
+    gatBoxPlot = 2
+
+script = os.path.basename(sys.argv[0])
+options = "hs:qa:d:i:t:"
+default_graph_type = GraphType.gatPerDuration
+
+
+def usage():
+    print("usage:")
+    print("{} [{}]".format(script, options))
+    print("options:")
+    print("  h          print this usage info")
+    print("  a<alg>     specify an algorithm, one per switch")
+    print("  d<domain>  specify domain")
+    print("  i<name>    specify instance name")
+    print("  s<file>    save to file")
+    print("  t<type>    specify type of plot; default {}".format(default_graph_type.name))
+    print("  q          quiet mode; no logging or graph showing")
 
 
 def open_connection():
@@ -111,11 +137,78 @@ def plot_gat_boxplots(db, algorithms, domain, instance):
 
 
 if __name__ == '__main__':
+    save_file = None
+    quiet = False
+    algorithms = []
+    domain = None
+    instance = None
+    graph_type = default_graph_type
+
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], options)
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif opt in ('-a', '--algorithm'):
+            algorithms.append(arg)
+        elif opt in ('-d', '--domain'):
+            domain = arg
+        elif opt in ('-i', '--instance'):
+            instance = arg
+        elif opt in ('-t', '--type'):
+            graph_type = getattr(GraphType, arg)
+        elif opt in ('-s', '--save'):
+            save_file = arg
+        elif opt in ('-q', '--quiet'):
+            quiet = True
+        else:
+            print("invalid switch '%s'" % opt)
+            usage()
+            sys.exit(2)
+
+    if not algorithms:
+        print("Must provide at least 1 algorithm")
+        usage()
+        sys.exit(2)
+    elif domain is None or instance is None:
+        print("Must provide domain and instance")
+        usage()
+        sys.exit(2)
+
     db = open_connection()
-    print_counts(db)
-    plot_gat_duration_error(db, ["LSS_LRTA_STAR",
-                                 "RTA_STAR",
-                                 "DYNAMIC_F_HAT"], "GRID_WORLD", "input/vacuum/dylan/slalom.vw").show()
+    if not quiet:
+        print_counts(db)
+
+    plotter = {
+        GraphType.gatPerDuration: lambda: plot_gat_duration_error(db, algorithms, domain, instance),
+        GraphType.gatBoxPlot: lambda: plot_gat_boxplots(db, algorithms, domain, instance)
+    }[graph_type]
+
+    plot = plotter()
+
+    # Save before showing since show resets the figures
+    if save_file is not None:
+        filename, ext = os.path.splitext(save_file)
+        if ext is '.pdf':
+            from matplotlib.backends.backend_pdf import PdfPages
+            pp = PdfPages(save_file)
+            plot.savefig(pp, format='pdf')
+            pp.close()
+        else:
+            # Try and save it
+            plot.savefig(save_file)
+
+    if not quiet:
+        plot.show()
+
+    # plot_gat_duration_error(db, ["LSS_LRTA_STAR",
+    #                              "RTA_STAR",
+    #                              "DYNAMIC_F_HAT"], "GRID_WORLD", "input/vacuum/dylan/slalom.vw")
     # plot_gat_boxplots(db, ["LSS_LRTA_STAR",
     #                        "RTA_STAR",
     #                        "DYNAMIC_F_HAT"], "GRID_WORLD", "input/vacuum/dylan/slalom.vw").show()
