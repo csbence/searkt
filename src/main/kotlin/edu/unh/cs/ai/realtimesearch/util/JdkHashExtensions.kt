3e1c5351875229ae056a18bf2fbae1314a408cc8
Bence Cserna <bence@cserna.net>
2016-04-07 14:45:10 -0400

    Add public resize to JDK hash- map and set

---
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/util/JdkHashExtensions.kt
@@ -0,0 +1,30 @@
+package edu.unh.cs.ai.realtimesearch.util
+
+import java.util.*
+
+/**
+ * @author Bence Cserna (bence@cserna.net)
+ */
+fun <K, V> HashMap<K, V>.resize(): HashMap<K, V> {
+    val nodesClass = javaClass
+
+    val resizeMethod = nodesClass.declaredMethods.first {
+        it.name == "resize"
+    }
+
+    resizeMethod.isAccessible = true
+    resizeMethod.invoke(this)
+    return this
+}
+
+fun <K> HashSet<K>.resize(): HashSet<K> {
+    val hashSetClass = javaClass
+
+    val internalMapField = hashSetClass.getDeclaredField("map")
+    internalMapField.isAccessible = true
+
+    val internalMapInstance = internalMapField.get(this) as HashMap<K, *>
+    internalMapInstance.resize()
+
+    return this
+}
