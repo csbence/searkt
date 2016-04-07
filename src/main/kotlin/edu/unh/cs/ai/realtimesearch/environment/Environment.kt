6bbf8f435f1df4db3887e123c489b24740500533
Bence Cserna <bence@cserna.net>
2016-04-07 00:06:28 -0400

    Clean up environment

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/Environment.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/Environment.kt
@@ -10,25 +10,25 @@ interface Environment<StateType : State<StateType>> {
      * Performs a step according to action a and current (maintained state)
      * @param action is the action taken
      */
-    public fun step(action: Action)
+    fun step(action: Action)
 
     /**
      * Returns current state
      * @return the current state of the environment
      */
-    public fun getState(): StateType
-    public fun getGoal(): StateType
+    fun getState(): StateType
+    fun getGoal(): StateType
 
     /**
      * Returns whether world has been solved
      *
      * @return true if goal has been reached
      */
-    public fun isGoal(): Boolean
+    fun isGoal(): Boolean
 
     /**
      * Resets the environment for a new experiment. Any data or changes
      * made during steps are to be cleared and reset to initial.
      */
-    public fun reset()
+    fun reset()
 }
