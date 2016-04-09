79423af59f547785fc96d23ac0154e360fa93d9a
Bence Cserna <bence@cserna.net>
2016-04-09 19:57:29 -0400

    Fix PointRobotStateTest imports

--- src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/pointrobot/PointRobotStateTest.kt
+++ src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/pointrobot/PointRobotStateTest.kt
@@ -1,5 +1,6 @@
 package edu.unh.cs.ai.realtimesearch.environment.pointrobot
 
+import edu.unh.cs.ai.realtimesearch.environment.pointrobotlost.PointRobotLOSTState
 import org.junit.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
