304d61cdbbc27a6aac094acd84f57ac7e4e66564
Bence Cserna <bence@cserna.net>
2016-04-15 16:05:54 -0400

    Fix theme colors

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/ThemeColors.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/visualizer/ThemeColors.kt
@@ -7,31 +7,31 @@ import javafx.scene.paint.Color.*
  * @author Bence Cserna (bence@cserna.net)
  */
 enum class ThemeColors(val color: Color, val stroke: Color = color, val opacity: Double = 1.0) {
+
+    UNH_PRIMARY_BLUE (Color.rgb(0, 53, 148)),
+    UNH_WHITE (Color.WHITE),
+    UNH_SECONDARY_BLUE (Color.rgb(37, 55, 70)),
+    UNH_SECONDARY_GRAY (Color.rgb(162, 170, 173)),
+    UNH_SECONDARY_SAND (Color.rgb(214, 210, 196)),
+    UNH_ACCENT (Color.rgb(247, 122, 5)),
+
     // General
-    ERROR_TEXT(RED),
-    BACKGROUND(WHITE),
+    ERROR_TEXT(Color.RED),
+    BACKGROUND(UNH_WHITE.color),
 
     // Grid based
-    GRID(UNH_SECODARY_GRAY),
-    OBSTACLE(UNH_SECODARY_BLUE),
-    PATH(INDIANRED),
-    AGENT(UNH_ACCENT),
-    START(UNH_PRIMARY_BLUE),
-    GOAL(UNH_PRIMARY_BLUE),
-    GOAL_CIRCLE(WHITE, UNH_PRIMARY_BLUE, 0.5),
-    EMPTY_SPACE(UNH_WHITE, UNH_WHITE),
+    GRID(UNH_SECONDARY_GRAY.color),
+    OBSTACLE(UNH_SECONDARY_BLUE.color),
+    PATH(Color.INDIANRED),
+    AGENT(UNH_ACCENT.color),
+    START(UNH_PRIMARY_BLUE.color),
+    GOAL(UNH_PRIMARY_BLUE.color),
+    GOAL_CIRCLE(WHITE, UNH_PRIMARY_BLUE.color, 0.5),
+    EMPTY_SPACE(UNH_WHITE.color, UNH_WHITE.color),
 
     // Acrobot
     LINK(BLACK, BLACK, 0.5),
     JOINT(RED);
 
-    companion object {
-        val UNH_PRIMARY_BLUE = Color.rgb(0, 53, 148)
-        val UNH_WHITE = Color.WHITE
-        val UNH_SECODARY_BLUE = Color.rgb(37, 55, 70)
-        val UNH_SECODARY_GRAY = Color.rgb(162, 170, 173)
-        val UNH_SECODARY_SAND = Color.rgb(214,210, 196)
-        val UNH_ACCENT = Color.rgb(247, 122, 5)
-    }
 
 }
