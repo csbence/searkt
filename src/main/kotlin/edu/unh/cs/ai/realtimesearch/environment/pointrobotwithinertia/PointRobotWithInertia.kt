f3b872543c18b651d713503b189cccff8af18581
Bence Cserna <bence@cserna.net>
2017-01-20 14:58:58 -0500

    Simplity PointRobotWithInertia

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/pointrobotwithinertia/PointRobotWithInertia.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/environment/pointrobotwithinertia/PointRobotWithInertia.kt
@@ -4,6 +4,7 @@ import edu.unh.cs.ai.realtimesearch.environment.Domain
 import edu.unh.cs.ai.realtimesearch.environment.SuccessorBundle
 import edu.unh.cs.ai.realtimesearch.environment.location.DoubleLocation
 import edu.unh.cs.ai.realtimesearch.environment.location.Location
+import java.lang.Math.*
 import java.util.*
 
 /**
@@ -21,23 +22,23 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
         val defaultStateFraction = 0.5
     }
 
-    val numAction = numActions; // total number of accelerations avaliable in one direction
-    val fractions = actionFraction; // number of values between whole numbers i.e. How many actions should there be in the range [0,1)?
-    var maxAcc = -1.0;
+    val numAction = numActions // total number of accelerations avaliable in one direction
+    val fractions = actionFraction // number of values between whole numbers i.e. How many actions should there be in the range [0,1)?
+    var maxAcc = -1.0
     private var actions = getAllActions()
 
     fun getAllActions(): ArrayList<PointRobotWithInertiaAction> {
-        var actions = ArrayList<PointRobotWithInertiaAction>()
+        val actions = ArrayList<PointRobotWithInertiaAction>()
         //        for (itX in 0..4) {
         //            for (itY in 0..4) {
         //                var xDoubleDot = ((itX) - 2.0) / 4.0;
         //                var yDoubleDot = ((itY) - 2.0) / 4.0;
         for (x in 0..numAction - 1) {
             for (y in 0..numAction - 1) {
-                var xDoubleDot = ((x) - ((numAction - (1.0)) / 2)) / fractions;
-                var yDoubleDot = ((y) - ((numAction - (1.0)) / 2)) / fractions;
+                val xDoubleDot = ((x) - ((numAction - (1.0)) / 2)) / fractions
+                val yDoubleDot = ((y) - ((numAction - (1.0)) / 2)) / fractions
                 if (maxAcc < 0)
-                    maxAcc = -1 * xDoubleDot;
+                    maxAcc = -1 * xDoubleDot
                 //                                println("" + xDoubleDot + " " + yDoubleDot)
                 actions.add(PointRobotWithInertiaAction(xDoubleDot, yDoubleDot))
                 //                println(maxAcc);
@@ -52,7 +53,7 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
         val successors: MutableList<SuccessorBundle<PointRobotWithInertiaState>> = ArrayList(9)
 
         //        println(state)
-        for (it in actions) {
+        for ((xDoubleDot, yDoubleDot) in actions) {
             val nSteps = 100
             val dt = 1.0 / nSteps
             var valid = true
@@ -62,14 +63,14 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
             var ydot = state.ydot
 
             for (i in 1..nSteps) {
-                xdot += it.xDoubleDot * dt
-                ydot += it.yDoubleDot * dt
-                x += xdot * dt;
-                y += ydot * dt;
+                xdot += xDoubleDot * dt
+                ydot += yDoubleDot * dt
+                x += xdot * dt
+                y += ydot * dt
 
                 if (!isLegalLocation(x, y)) {
-                    valid = false;
-                    break;
+                    valid = false
+                    break
                 }
             }
 
@@ -77,9 +78,9 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
                 //                println0("" + x + " " + y + " " + (state.loc.x + state.xdot) + " " + (state.loc.y + state.ydot))
                 //                println("\t" + PointRobotWithInertiaState(x, y, state.xdot + it.xDoubleDot, state.ydot + it.yDoubleDot))
                 successors.add(SuccessorBundle(
-                        PointRobotWithInertiaState(x, y, state.xdot + it.xDoubleDot, state.ydot + it.yDoubleDot, stateFraction),
-                        PointRobotWithInertiaAction(it.xDoubleDot, it.yDoubleDot),
-                        actionDuration));
+                        PointRobotWithInertiaState(x, y, state.xdot + xDoubleDot, state.ydot + yDoubleDot, stateFraction),
+                        PointRobotWithInertiaAction(xDoubleDot, yDoubleDot),
+                        actionDuration))
             }
         }
 
@@ -109,101 +110,90 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
     * */
     override fun heuristic(state: PointRobotWithInertiaState): Double {
         //Distance Formula
-        var bx = state.xdot
-        var cx = state.x - endLocation.x
+        val bx = state.xdot
+        val cx = state.x - endLocation.x
 
-        var by = state.ydot
-        var cy = state.y - endLocation.y
+        val by = state.ydot
+        val cy = state.y - endLocation.y
 
-        var resultx1 = quadraticFormula(0.5 * maxAcc, bx, cx)
-        var resultx2 = quadraticFormula(-0.5 * maxAcc, bx, cx)
+        val resultx1 = quadraticFormula(0.5 * maxAcc, bx, cx)
+        val resultx2 = quadraticFormula(-0.5 * maxAcc, bx, cx)
 
-        var resulty1 = quadraticFormula(0.5 * maxAcc, by, cy)
-        var resulty2 = quadraticFormula(-0.5 * maxAcc, by, cy)
+        val resulty1 = quadraticFormula(0.5 * maxAcc, by, cy)
+        val resulty2 = quadraticFormula(-0.5 * maxAcc, by, cy)
 
         //        println("" + resultx1 + " " + resultx2 + " "+ resulty1 + " " + resulty2 + " "
         //                + Math.max(Math.min(resultx1, resultx2), Math.min(resulty1, resulty2)))
 
-        var minx = Math.min(resultx1, resultx2)
-        var miny = Math.min(resulty1, resulty2)
+        val minx = min(resultx1, resultx2)
+        val miny = min(resulty1, resulty2)
 
-        var retval: Double
+        val retval: Double
 
         if (minx == Double.MAX_VALUE && miny != Double.MAX_VALUE)
-            retval = miny;
+            retval = miny
         else if (minx != Double.MAX_VALUE && miny === Double.MAX_VALUE)
             retval = minx
         else if (minx == Double.MAX_VALUE && miny == Double.MAX_VALUE)
-            retval = 0.0;
+            retval = 0.0
         else
-            retval = Math.max(minx, miny)
+            retval = max(minx, miny)
         //        println(retval)
         return retval * actionDuration
     }
 
     override fun heuristic(startState: PointRobotWithInertiaState, endState: PointRobotWithInertiaState): Double {
         //Distance Formula
-        var bx = startState.xdot
-        var cx = startState.x - endState.x
+        val bx = startState.xdot
+        val cx = startState.x - endState.x
 
-        var by = startState.ydot
-        var cy = startState.y - endState.y
+        val by = startState.ydot
+        val cy = startState.y - endState.y
 
-        var resultx1 = quadraticFormula(0.5, bx, cx)
-        var resultx2 = quadraticFormula(-0.5, bx, cx)
+        val resultx1 = quadraticFormula(0.5, bx, cx)
+        val resultx2 = quadraticFormula(-0.5, bx, cx)
 
-        var resulty1 = quadraticFormula(0.5, by, cy)
-        var resulty2 = quadraticFormula(-0.5, by, cy)
+        val resulty1 = quadraticFormula(0.5, by, cy)
+        val resulty2 = quadraticFormula(-0.5, by, cy)
 
-        //        println("" + resultx1 + " " + resultx2 + " "+ resulty1 + " " + resulty2 + " "
-        //                + Math.max(Math.min(resultx1, resultx2), Math.min(resulty1, resulty2)))
+        val minx = min(resultx1, resultx2)
+        val miny = min(resulty1, resulty2)
 
-        var minx = Math.min(resultx1, resultx2)
-        var miny = Math.min(resulty1, resulty2)
-
-        var retval: Double
-
-        if (minx == Double.MAX_VALUE && miny != Double.MAX_VALUE)
-            retval = miny;
-        else if (minx != Double.MAX_VALUE && miny === Double.MAX_VALUE)
-            retval = minx
-        else if (minx == Double.MAX_VALUE && miny == Double.MAX_VALUE)
-            retval = 0.0;
-        else
-            retval = Math.max(minx, miny)
-        //        println(retval)
-        return retval * actionDuration
+        return actionDuration * when {
+            minx == Double.MAX_VALUE && miny != Double.MAX_VALUE -> miny
+            minx != Double.MAX_VALUE && miny === Double.MAX_VALUE -> minx
+            minx == Double.MAX_VALUE && miny == Double.MAX_VALUE -> 0.0
+            else -> max(minx, miny)
+        }
     }
 
     fun quadraticFormula(a: Double, b: Double, c: Double): Double {
-        if (Math.pow(b, 2.0) - 4 * a * c < 0.0)
+        if (pow(b, 2.0) - 4 * a * c < 0.0)
             return Double.MAX_VALUE
 
-        var result1 = -1 * b + Math.sqrt(Math.pow(b, 2.0) - 4 * a * c)
+        var result1 = -1 * b + sqrt(pow(b, 2.0) - 4 * a * c)
         result1 /= (2 * a)
 
-        var result2 = -1 * b - Math.sqrt(Math.pow(b, 2.0) - 4 * a * c)
+        var result2 = -1 * b - sqrt(pow(b, 2.0) - 4 * a * c)
         result2 /= (2 * a)
-        if (result1 < 0.0 && result2 >= 0.0)
-            return result2;
-        if (result2 < 0.0 && result1 >= 0.0)
-            return result1;
 
-        if (result1 < 0.0 && result2 < 0.0)
-            return Double.MAX_VALUE;
-
-        return Math.max(result1, result2)
+        return when {
+            result1 < 0.0 && result2 >= 0.0 -> result2
+            result2 < 0.0 && result1 >= 0.0 -> result1
+            result1 < 0.0 && result2 < 0.0 -> Double.MAX_VALUE
+            else -> max(result1, result2)
+        }
     }
 
     override fun distance(state: PointRobotWithInertiaState): Double {
         //Distance Formula
-        return (Math.sqrt(
-                Math.pow((endLocation.x) - state.x, 2.0)
-                        + Math.pow((endLocation.y) - state.y, 2.0)) - goalRadius)
+        return (sqrt(
+                pow((endLocation.x) - state.x, 2.0)
+                        + pow((endLocation.y) - state.y, 2.0)) - goalRadius)
     }
 
     override fun isGoal(state: PointRobotWithInertiaState): Boolean {
-        return distance(state) <= 0 && state.xdot == 0.0 && state.ydot == 0.0;
+        return distance(state) <= 0 && state.xdot == 0.0 && state.ydot == 0.0
     }
 
     override fun print(state: PointRobotWithInertiaState): String {
@@ -243,14 +233,14 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
             var ydot = state.ydot
 
             for (i in 1..nSteps) {
-                x -= xdot * dt;
-                y -= ydot * dt;
+                x -= xdot * dt
+                y -= ydot * dt
                 xdot -= it.xDoubleDot * dt
                 ydot -= it.yDoubleDot * dt
 
                 if (!isLegalLocation(x, y)) {
-                    valid = false;
-                    break;
+                    valid = false
+                    break
                 }
             }
 
@@ -259,7 +249,7 @@ class PointRobotWithInertia(val width: Int, val height: Int, val blockedCells: S
                 predecessors.add(SuccessorBundle(
                         PointRobotWithInertiaState(x, y, state.xdot - it.xDoubleDot, state.ydot - it.yDoubleDot, stateFraction),
                         PointRobotWithInertiaAction(it.xDoubleDot, it.yDoubleDot),
-                        actionDuration));
+                        actionDuration))
             }
         }
         return predecessors
