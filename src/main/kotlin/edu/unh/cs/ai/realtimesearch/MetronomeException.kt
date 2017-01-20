c4961759fa344ac3e06c66ce17f82eecc92e99a4
Bence Cserna <bence@cserna.net>
2017-01-20 15:02:05 -0500

    Add MetronomeException

---
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/MetronomeException.kt
@@ -0,0 +1,6 @@
+package edu.unh.cs.ai.realtimesearch
+
+/**
+ * @author Bence Cserna (bence@cserna.net)
+ */
+open class MetronomeException(message: String?, cause: Throwable?) : RuntimeException(message, cause)
