7adc7ecdb293473609ceb7948ce480057fdf7088
Bence Cserna <bence@cserna.net>
2017-01-25 13:35:45 -0500

    Fix raceTrack action tests

--- src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/racetrack/RaceTrackActionTest.kt
+++ src/test/kotlin/edu/unh/cs/ai/realtimesearch/environment/racetrack/RaceTrackActionTest.kt
@@ -1,8 +1,6 @@
 package edu.unh.cs.ai.realtimesearch.environment.racetrack
 
-import edu.unh.cs.ai.realtimesearch.environment.location.Location
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
@@ -11,40 +9,42 @@ import org.junit.Test
 class RaceTrackActionTest {
 
     @Test
-    fun testGetIndex() {
-        var action = RaceTrackAction.DOWN
-
-        assertTrue(action.index == 7)
-    }
-
-    @Test
     fun testGetRelativeLocation() {
         var action = RaceTrackAction.DOWN
-        assertEquals(action.getAcceleration(), Location(0, -1))
+        assertEquals(action.aX, 0)
+        assertEquals(action.aY, -1)
 
         action = RaceTrackAction.UP
-        assertEquals(action.getAcceleration(), Location(0, 1))
+        assertEquals(action.aX, 0)
+        assertEquals(action.aY, 1)
 
         action = RaceTrackAction.NO_OP
-        assertEquals(action.getAcceleration(), Location(0, 0))
+        assertEquals(action.aX, 0)
+        assertEquals(action.aY, 0)
 
         action = RaceTrackAction.LEFT
-        assertEquals(action.getAcceleration(), Location(-1, 0))
+        assertEquals(action.aX, -1)
+        assertEquals(action.aY, 0)
 
         action = RaceTrackAction.RIGHT
-        assertEquals(action.getAcceleration(), Location(1, 0))
+        assertEquals(action.aX, 1)
+        assertEquals(action.aY, 0)
 
         action = RaceTrackAction.LEFT_UP
-        assertEquals(action.getAcceleration(), Location(-1, 1))
+        assertEquals(action.aX, -1)
+        assertEquals(action.aY, 1)
 
         action = RaceTrackAction.LEFT_DOWN
-        assertEquals(action.getAcceleration(), Location(-1, -1))
+        assertEquals(action.aX, -1)
+        assertEquals(action.aY, -1)
 
         action = RaceTrackAction.RIGHT_DOWN
-        assertEquals(action.getAcceleration(), Location(1, -1))
+        assertEquals(action.aX, 1)
+        assertEquals(action.aY, -1)
 
         action = RaceTrackAction.RIGHT_UP
-        assertEquals(action.getAcceleration(), Location(1, 1))
+        assertEquals(action.aX, 1)
+        assertEquals(action.aY, 1)
     }
 
 }
