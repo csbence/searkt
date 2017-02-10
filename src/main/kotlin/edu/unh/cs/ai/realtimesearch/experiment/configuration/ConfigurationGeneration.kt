52f3a32f0c41116a1aba2f7c8c1cd9642088e201
Bence Cserna <bence@cserna.net>
2017-02-10 14:30:16 -0500

    Add algorithm specific multipliers for configuration generation

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/experiment/configuration/ConfigurationGeneration.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/experiment/configuration/ConfigurationGeneration.kt
@@ -1,13 +1,10 @@
 package edu.unh.cs.ai.realtimesearch.experiment.configuration
 
-import edu.unh.cs.ai.realtimesearch.actionDurations
 import edu.unh.cs.ai.realtimesearch.environment.Domains
 import edu.unh.cs.ai.realtimesearch.experiment.configuration.realtime.LookaheadType
 import edu.unh.cs.ai.realtimesearch.experiment.configuration.realtime.TerminationType
 import edu.unh.cs.ai.realtimesearch.planner.CommitmentStrategy
 import edu.unh.cs.ai.realtimesearch.planner.Planners
-import edu.unh.cs.ai.realtimesearch.terminationType
-import edu.unh.cs.ai.realtimesearch.timeLimit
 
 /**
  * @author Bence Cserna (bence@cserna.net)
@@ -31,13 +28,12 @@ fun generateConfigurations(
         actionDurations: Iterable<Long>,
         terminationType: TerminationType,
         lookaheadType: LookaheadType,
-        timeLimit: Long): Collection<GeneralExperimentConfiguration> {
+        timeLimit: Long,
+        plannerExtras: Iterable<Triple<Planners, String, Iterable<Any>>>? = null): Collection<GeneralExperimentConfiguration> {
 
-    var configurations: MutableCollection<Map<String, Any>> = mutableListOf()
-
-    domains.mapTo(configurations, {
-        mutableMapOf(Configurations.DOMAIN_NAME.toString() to it.first.toString(), Configurations.DOMAIN_PATH.toString() to it.second)
-    })
+    var configurations: Collection<Map<String, Any>> = domains.map {
+        mapOf(Configurations.DOMAIN_NAME.toString() to it.first.toString(), Configurations.DOMAIN_PATH.toString() to it.second)
+    }
 
     configurations = configurations.cartesianProduct(Configurations.ALGORITHM_NAME.toString(), planners.map(Any::toString)).toMutableList()
     configurations = configurations.cartesianProduct(Configurations.COMMITMENT_STRATEGY.toString(), commitmentStrategy.map(Any::toString)).toMutableList()
@@ -45,6 +41,14 @@ fun generateConfigurations(
     configurations = configurations.cartesianProduct(Configurations.TERMINATION_TYPE.toString(), listOf(terminationType.toString())).toMutableList()
     configurations = configurations.cartesianProduct(Configurations.LOOKAHEAD_TYPE.toString(), listOf(lookaheadType.toString())).toMutableList()
     configurations = configurations.cartesianProduct(Configurations.TIME_LIMIT.toString(), listOf(timeLimit)).toMutableList()
+
+    // Apply planner specific extras
+    plannerExtras?.forEach { (planner, key, values) ->
+        val irrelevantConfigurations = configurations.filter { it[Configurations.ALGORITHM_NAME.toString()] != planner.toString() }
+        val relevantConfigurations = configurations.filter { it[Configurations.ALGORITHM_NAME.toString()] == planner.toString() }
+
+        configurations = irrelevantConfigurations + relevantConfigurations.cartesianProduct(key, values).toMutableList()
+    }
 
     return configurations.map { GeneralExperimentConfiguration(HashMap(it)) }
 }
