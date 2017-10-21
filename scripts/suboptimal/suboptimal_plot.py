eb17f17f254b6e4ae9483eb6971bb98c1caa4b36
William Doyle <williamdoyleiii@gmail.com>
2017-10-21 08:31:15 -0400

    update plotting

--- scripts/suboptimal/suboptimal_plot.py
+++ scripts/suboptimal/suboptimal_plot.py
@@ -1,4 +1,8 @@
 import simplejson as json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
 from pprint import pprint
 
 json_file = 'results.json'
@@ -6,9 +10,23 @@ json_data = open(json_file)
 data = json.load(json_data)
 json_data.close()
 
-print(data[0].keys())
-print(data[0]['goalAchievementTime'])
-print(data[1]['goalAchievementTime'])
+for key in data[0].keys():
+    print(key)
+
+df = pd.DataFrame()
+df['x'] = [2.0, 2.0]
+df['y'] = [data[0]['goalAchievementTime'], data[1]['goalAchievementTime']]
+
+sns.set_style('darkgrid')
+ax = sns.pointplot(x="Suboptimality_Bound", y="Nodes_Expanded", data=df)
+
+print(str(data[0]['configuration']))
+config_dict = data[0]['configuration']
+print(str(data_config['algorithmName']))
 print(str(data[0]['planningTime']/1000000.0) + 'ms')
+print(str(data[0]['goalAchievementTime']) + ' nodes expanded')
+
+
 print(str(data[1]['planningTime']/1000000.0) + 'ms')
+print(str(data[1]['goalAchievementTime']) + ' nodes expanded')
 
