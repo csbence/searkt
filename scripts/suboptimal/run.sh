cc70a1d69f7c3f9705f071845c5e4cd417afe247
William Doyle <williamdoyleiii@gmail.com>
2017-10-26 18:08:11 -0400

    added exp run file

---
+++ scripts/suboptimal/run.sh
@@ -0,0 +1,14 @@
+sendSlackNotification.bash "#experiments" "experiment_bot" "William just started running experiments."
+
+echo "Running Experiments!"
+
+echo "Test 1..."
+
+echo "Test 2..."
+
+echo "Test 3..."
+
+echo "Exiting!"
+
+sendSlackNotification.bash "#experiments" "experiment_bot" "William's experiments just finished."
+
