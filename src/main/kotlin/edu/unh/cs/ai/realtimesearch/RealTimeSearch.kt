5c0e4408249a518a9f05e57a07b8ba5d385a5ce8
William Doyle <williamdoyleiii@gmail.com>
2017-11-20 19:41:30 -0500

    SIMPLE_SAFE no longer exhibits previous error behavior of past results

--- src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
+++ src/main/kotlin/edu/unh/cs/ai/realtimesearch/RealTimeSearch.kt
@@ -32,10 +32,10 @@ fun main(args: Array<String>) {
             domains = listOf(
 //                      Domains.SLIDING_TILE_PUZZLE_4 to "input/tiles/korf/4/real/12"
 //                    Domains.GRID_WORLD to "input/vacuum/empty.vw"
-                    Domains.RACETRACK to "input/racetrack/hansen-bigger-quad.track"
-//                    Domains.RACETRACK to "input/racetrack/barto-big.track"
-//                    Domains.RACETRACK to "input/racetrack/uniform.track",
-//                    Domains.RACETRACK to "input/racetrack/barto-small.track"
+                    Domains.RACETRACK to "input/racetrack/hansen-bigger-quad.track",
+                    Domains.RACETRACK to "input/racetrack/barto-big.track",
+                    Domains.RACETRACK to "input/racetrack/uniform.track",
+                    Domains.RACETRACK to "input/racetrack/barto-small.track"
 //                    TRAFFIC to "input/traffic/vehicle0.v"
             ),
 //            domains = (88..88).map { TRAFFIC to "input/traffic/50/traffic$it" },
