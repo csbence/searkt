8be81686c6d4caac75d8b77b381c74518aa2d396
William Doyle <williamdoyleiii@gmail.com>
2018-01-17 18:09:25 -0500

    added a way to configure algorithm specific configuration settings to the script and added a simple way of making domain specific configurations - a sample for racetrack is provided

--- scripts/searkt.py
+++ scripts/searkt.py
@@ -5,16 +5,32 @@
 import copy
 import json
 
-algorithmsToRun = ['WEIGHTED_A_STAR']
+algorithmsToRun = ['WEIGHTED_A_STAR', 'SAFE_REAL_TIME_SEARCH']
+weight = [3.0]
+actionDuration = [1000]
+expansionLimit = [1000000]
+lookaheadType = ['DYNAMIC']
+timeLimit = [120000000]
 actionDurations = [1, 5, 10]
+
+defaultConfiguration = dict()
+defaultConfiguration['algorithmName'] = algorithmsToRun
+defaultConfiguration['actionDuration'] = actionDuration
+defaultConfiguration['expansionLimit'] = expansionLimit
+defaultConfiguration['lookaheadType'] = lookaheadType
+defaultConfiguration['actionDuration'] = actionDurations
 
 def generate_racetrack():
     configurations = [{}] 
 
-    
     racetracks = ['uniform.track', 'long.track']
-
     configurations = cartesian_product(configurations, 'domainName', ['RACETRACK'])
+
+    for key, value in defaultConfiguration.items():
+        configurations = cartesian_product(configurations, key, value)
+
+    configurations = cartesian_product(configurations, 'weight', weight, 'algorithmName', 'WEIGHTED_A_STAR')
+    return configurations
 
 
 
@@ -32,19 +48,28 @@ def generate_configurations():
     return configurations
 
 
-def cartesian_product(base, key, values):
+def cartesian_product(base, key, values, filter_key = '', filter_value = ''):
     new_base = []
-    for item in base:
-        for value in values:
-            new_configuration = copy.deepcopy(item)
-            new_configuration[key] = value
-            new_base.append(new_configuration)
+    if filter_key == '' and filter_value ==  '':
+       for item in base:
+            for value in values:
+                new_configuration = copy.deepcopy(item)
+                new_configuration[key] = value
+                new_base.append(new_configuration)
+    else:
+        new_base = copy.deepcopy(base)
+        for item in new_base:
+            iterCopy = dict(item)
+            for a_key, value in iterCopy.items():
+                if str(a_key) == str(filter_key) and filter_value in item[str(a_key)]:
+                    item[str(key)] = values
 
     return new_base
 
 
 def main():
     configurations = generate_configurations()
+    configurations = generate_racetrack()
     print(json.dumps(configurations))
     pass
 
