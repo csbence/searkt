55886b1a541eb1c9191a4c5ed1dff211abc2de37
William Doyle <williamdoyleiii@gmail.com>
2018-04-27 13:34:26 -0400

    format and changed for python standard function names

--- scripts/view_data.py
+++ scripts/view_data.py
@@ -53,7 +53,7 @@ def process_data():
                     expandedNodes = instance["expandedNodes"]
                     totalNodes = generatedNodes + expandedNodes
                     runTime = instance["experimentRunTime"]
-                    data_dict["nodesPerSecond"].append((expandedNodes)/runTime)
+                    data_dict["nodesPerSecond"].append((expandedNodes) / runTime)
                 else:
                     data_dict["nodesPerSecond"].append(0.0)
                 i = i + 1
@@ -76,10 +76,12 @@ def process_data():
     print('Failed experiments {}'.format(failed_experiments))
     return data_dict
 
-def extractLowerTwo(num):
+
+def extract_tens_place(num):
     ones = num % 10
     tens = int((num / 10) % 10)
     return ones + (10 * tens)
+
 
 def filter_nodes_generated(df):
     over_five_million = dict()
@@ -90,7 +92,7 @@ def filter_nodes_generated(df):
     over_five_million["EESD"] = 0
     over_five_million["EECS"] = 0
     offset = []
-    for x in range(0,100):
+    for x in range(0, 100):
         offset.append(99 - x)
     drop_index = []
     for key in df.keys():
@@ -106,9 +108,9 @@ def filter_nodes_generated(df):
             index = 0
             for item in df[key]:
                 alg_success = df.iloc[index, df.columns.get_loc("success")]
-                assert(item == alg_success)
+                assert (item == alg_success)
                 if not alg_success:
-                    li = extractLowerTwo(index)
+                    li = extract_tens_place(index)
                     alg = df.iloc[index, df.columns.get_loc("algorithm")]
                     weight = df.iloc[index, df.columns.get_loc("weight")]
                     print(alg + "@" + str(weight))
@@ -116,7 +118,7 @@ def filter_nodes_generated(df):
                     upper = index + offset[li]
                     lower = index - offset[-li] - 1
                     print("\tdropping {}->{}".format(lower, upper))
-                    for i in range(lower, upper+1):
+                    for i in range(lower, upper + 1):
                         drop_index.append(i)
                 index = index + 1
         # if key == "generatedNodes":
@@ -144,22 +146,24 @@ def filter_nodes_generated(df):
             for item in df[key]:
                 seconds = df.iloc[index, df.columns.get_loc("experimentRunTime")]
                 # if seconds >10:
-                    # print(seconds)
+                # print(seconds)
                 if seconds != 0:
                     df.iloc[index, df.columns.get_loc("experimentRunTime")] = math.log10(seconds)
                 index = index + 1
     return over_five_million, drop_index
 
 
-def lookUpInDataFrame(df, num):
+def look_up_in_df(df, num):
     a = df.iloc[num, df.columns.get_loc("algorithm")]
     w = df.iloc[num, df.columns.get_loc("weight")]
     print(str(a) + "@" + str(w))
+
 
 def filter_failed_sets(instance_set):
     if instance_set.success.all():
         return instance_set
     return None
+
 
 def plot(data_dict):
     # print(data_dict)
@@ -172,15 +176,15 @@ def plot(data_dict):
     sns.set_context("paper")
     # display(df[(df.generatedNodes > 5000000) & (df.success == True) & (df.algorithm == "DPS")]['generatedNodes'])
     over_five_million, drop_index = filter_nodes_generated(df)
-    drop_set  = set(drop_index)
+    drop_set = set(drop_index)
     print(drop_set)
 
     df2 = df.groupby(["weight", "algorithm"]).apply(filter_failed_sets)
-    lookUpInDataFrame(df, 3798)
-    lookUpInDataFrame(df, 3799)
-    lookUpInDataFrame(df, 3800)
-    lookUpInDataFrame(df, 5700)
-    lookUpInDataFrame(df, 7600)
+    look_up_in_df(df, 3798)
+    look_up_in_df(df, 3799)
+    look_up_in_df(df, 3800)
+    look_up_in_df(df, 5700)
+    look_up_in_df(df, 7600)
     # unit for paper
     # df.drop(df.index[range(5700, 6100)], inplace=True)
     # df.drop(df.index[range(7200, 7500)], inplace=True)
@@ -203,7 +207,6 @@ def plot(data_dict):
     plt.tight_layout()
     sns.despine(ax=success_plot)
     plt.figure()
- 
 
     # axes = success_plot.axes
     # axes.set(ylim=(0, 1.01))
@@ -220,17 +223,14 @@ def plot(data_dict):
     plt.ylabel('log10(generatedNodes')
     plt.figure()
 
-
-
     df_gen = df2[df2.success == True]
     # df = df[(df.generatedNodes >= 5000000) & (df.success == True)]
     # expand_plot = sns.pointplot(x="weight", y="expandedNodes", hue="algorithm", data=df_gen, capsize=0.1,
     #                             palette="Set2")
 
-
     # plt.title('Nodes Expanded')
     # plt.figure()
- 
+
     # sns.set_context("paper")
     # sns.set_style("dark", {"axes.facecolor": ".9"})
     # success_plot = sns.pointplot(x="instance", y="success", hue="algorithm", data=df, capsize=.2)
@@ -240,14 +240,16 @@ def plot(data_dict):
 
     # axes = expand_plot.axes
     # axes.set(ylim=(0,2000000))
-    success_plot = sns.pointplot(x="weight", y="experimentRunTime", hue="algorithm", data=df_gen, capsize=.1, palette="Set2")
+    success_plot = sns.pointplot(x="weight", y="experimentRunTime", hue="algorithm", data=df_gen, capsize=.1,
+                                 palette="Set2")
     plt.title('CPU time')
     plt.ylabel('log10(experimentRunTime)')
     sns.despine(ax=success_plot)
     plt.savefig('runtime-unit.eps', format='eps')
     plt.figure()
 
-    success_plot = sns.pointplot(x="weight", y="nodesPerSecond", hue="algorithm", data=df_gen, capsize=0.1, palette="Set2")
+    success_plot = sns.pointplot(x="weight", y="nodesPerSecond", hue="algorithm", data=df_gen, capsize=0.1,
+                                 palette="Set2")
     plt.title('Nodes per Second')
     sns.despine(ax=success_plot)
     plt.savefig('nps-unit.eps', format='eps')
@@ -260,25 +262,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
