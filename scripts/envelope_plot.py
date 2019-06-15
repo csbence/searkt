a8fa1cf5f72fa8be8daa04d16161037fadc46b5e
Kevin C. Gall <kcg245@gmail.com>
2019-06-15 13:39:58 -0400

    Plotting notebook for envelope no output included in git

--- scripts/envelope_plot.py
+++ scripts/envelope_plot.py
@@ -2,33 +2,15 @@
 
 import json
 import gzip
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import re
-import seaborn as sns
-import argparse
-from matplotlib.backends.backend_pdf import PdfPages
 from pandas import DataFrame
 import statsmodels.stats.api as sms
 
-__author__ = 'Bence Cserna, modified by Kevin C. Gall'
+__author__ = 'Kevin C. Gall'
 
 alg_map = {"A_STAR": "A*", "LSS_LRTA_STAR": "LSS-LRTA*", "SAFE_RTS": "SRTS", "S_ZERO": "S0", "SIMPLE_SAFE": "SS",
            "SINGLE_SAFE": "BEST_SAFE", "SAFE_RTS_TOP": "SRTS_TOP", "TIME_BOUNDED_A_STAR": "TBA*", "CES": "CES",
            "BACK_ES": "Backward ES", "BI_ES": "Bidirectional ES"}
-
-
-def flatten(experiment):
-    experiment_configuration = experiment.pop('configuration')
-
-    return {**experiment, **experiment_configuration}
-
-
-def construct_data_frame(data):
-    flat_data = [flatten(experiment) for experiment in data]
-    return DataFrame(flat_data)
 
 
 def read_data(file_name):
@@ -40,193 +22,136 @@ def read_data(file_name):
         return json.load(file)
 
 
-def set_rc():
-    mpl.rcParams['axes.labelsize'] = 10
-    mpl.rcParams['xtick.top'] = True
-    mpl.rcParams['font.family'] = 'Serif'
+def construct_data_frame(data):
+    flat_data = [flatten(experiment) for experiment in data]
+    df = DataFrame(flat_data)
+    remove_unused_columns(df)
+
+    return df
+
+
+def flatten(experiment):
+    experiment_configuration = experiment.pop('configuration')
+
+    return {**experiment, **experiment_configuration}
+
+
+def remove_unused_columns(data):
+    data.drop(['actions', 'commitmentType', "success", "timeLimit",
+               "terminationType", 'timestamp', 'octileMovement', 'lookaheadType',
+               'firstIterationDuration',
+               "targetSelection", "safetyExplorationRatio", "safetyProof", "safetyWindowSize", "safetyBackup",
+               'domainSeed', 'averageVelocity', "proofSuccessful", "rawDomain", "anytimeMaxCount",
+               "systemProperties", "towardTopNode", "numberOfProofs"],
+              axis=1,
+              inplace=True,
+              errors='ignore')
+
+
+def analyze_within_optimal(exp, opt):
+    exp_df = construct_data_frame(exp)
+    opt_df = construct_data_frame(opt)
+
+    opt_df['optimalPathLength'] = opt_df['pathLength']
+    opt_df = opt_df[['domainPath', 'optimalPathLength']]
+
+    exp_df = pd.merge(exp_df, opt_df, how='inner', on=['domainPath'])
+    exp_df['withinOpt'] = exp_df['goalAchievementTime'] / (exp_df["actionDuration"] * exp_df["optimalPathLength"])
+
+    return exp_df
+
+
+def get_within_opt_results(data, group_by):
+    results = DataFrame(columns="actionDuration withinOpt algorithmName lbound rbound algCode".split())
+
+    # prep config dict by making first pass through group_by
+    config = {}
+    for gp_dict in group_by.values():
+        for column in gp_dict.keys():
+            if column not in config:
+                config[column] = list()
+
+    # Change data structure such that within optimal is averaged,
+    # grouped by action duration and algorithm as specified in group by options
+
+    for algorithm_name, custom_grouping_map in group_by.items():
+        alg_data = data[data['algorithmName'] == algorithm_name]
+        groupings = ['actionDuration']
+
+        for field in custom_grouping_map:
+            groupings.append(field)
+
+        for fields, duration_group in alg_data.groupby(groupings):
+            transformed_alg_name = algorithm_name
+            if transformed_alg_name in alg_map:
+                transformed_alg_name = alg_map[transformed_alg_name]
+
+            unused_config_keys = set(config.keys())
+            action_duration = fields
+            if len(groupings) > 1:
+                action_duration = fields[0]
+
+                for field_name, field_value in zip(groupings, fields):
+                    if field_name in custom_grouping_map:
+                        config[field_name].append(field_value)
+                        unused_config_keys.remove(field_name)
+
+                        transformed_alg_name += ' ' + custom_grouping_map[field_name] + ': ' + str(field_value)
+
+            for key in unused_config_keys:
+                config[key].append(None) # blank value
+
+            # Get mean of within optimal calculation, add row to results dataframe
+            mean_within_opt = duration_group['withinOpt'].mean()
+            within_opt_list = list(duration_group['withinOpt'])
+            bound = sms.DescrStatsW(within_opt_list).zconfint_mean()
+            results = add_row(results, [action_duration, mean_within_opt, transformed_alg_name,
+                                        abs(mean_within_opt - bound[0]), abs(mean_within_opt - bound[1]),
+                                        algorithm_name])
+
+    # extend df for custom config we want to filter on later
+    for column, value_list in config.items():
+        results[column] = value_list
+
+    return results
 
 
 def add_row(df, values):
     return df.append(dict(zip(df.columns.values, values)), ignore_index=True)
 
 
-def plot_domain_instances(data):
-    # Each domain configuration
-    for domain_path, domain_group in data.groupby(["domainPath"]):
-        domain_name = re.search("[^/]+$", domain_path).group(0).rstrip(".vw")
+def analyze_results(optimal_plans, experiments):
+    exp_data = []
+    for path_name in experiments:
+        exp_data += read_data(path_name)
 
-        plt.title(domain_name)
-        plt.ylabel('Goal Achievement Time (Factor of Optimal)')
-        plt.xlabel('Expansion Limit (Per Iteration)')
+    optimal_data = []
+    for optimal_path_name in optimal_plans:
+        optimal_data += read_data(optimal_path_name)
 
-        # expansion_series = domain_group["actionDuration"].unique()
+    df = analyze_within_optimal(exp_data, optimal_data)
 
-        # Each algorithm (different backlog ratios count as different algorithm)
-        palette = sns.color_palette(n_colors=10)
-        count = 0
-        for fields, alg_group in domain_group.groupby(['algorithmName', 'backlogRatio']):
-            alg_name = fields[0]
-            if fields[0] == "CES":
-                alg_name += " Backup Ratio: " + str(fields[1])
-
-            plt.plot('actionDuration', "withinOpt", data=alg_group, color=palette[count], label=alg_name)
-
-            count += 1
-
-        plt.legend()
-
-        plt.savefig("../output/" + domain_name + ".png", format="png")
-        plt.figure()
+    return df
 
 
-def plot_all_experiments(data, plot_title):
-    results = DataFrame(columns="actionDuration withinOpt algorithmName lbound rbound".split())
+# Prepare data for plotting - plot factor of optimal with confidence bounds grouped by algorithm and alg parameters.
+# Algorithms grouped by algorithm name by default, additionally grouped by parameters as specified in second parameter
+# data - The data
+# domain_tokens - optional array of domain tokens to split the results by
+# group_by - dict; each key is an algorithm name. Each value is another dict of key-value pairs: configuration to label.
+#           Note that if an algorithm name is not a key in group_by, it will not be returned in the results
+# returns if domain_tokens not passed, the results. If domain_tokens passed, a dict where each key is a token and each
+#           value is the result for that domain
+def prepare_for_within_opt_plot(data, domain_tokens=None, group_by=None):
+    if group_by is None:
+        group_by = {}
 
-    # for testing against self!
-    # data = data[data.algorithmName != 'LSS_LRTA_STAR']
-
-    # rescale action durations to ms
-    # data['actionDuration'] = data['actionDuration'] / 1000000
-
-    # Change data structure such that goal achievement time is averaged,
-    # grouped by action duration and algorithm
-    for fields, duration_group in data.groupby(['algorithmName', 'actionDuration']):
-        alg_name = fields[0]
-        if (alg_name in alg_map):
-            alg_name = alg_map[alg_name]
-
-        # Get mean of within optimal calculation, add row to results dataframe
-        mean_within_opt = duration_group['withinOpt'].mean()
-        within_opt_list = list(duration_group['withinOpt'])
-        bound = sms.DescrStatsW(within_opt_list).zconfint_mean()
-        results = add_row(results,
-                          [fields[1], mean_within_opt, alg_name, abs(mean_within_opt - bound[0]), abs(mean_within_opt - bound[1])])
-
-    errors = []
-    for alg, alg_group in results.groupby('algorithmName'):
-        errors.append([alg_group['lbound'].values, alg_group['rbound'].values])
-
-    pivot = results.pivot(index="actionDuration", columns="algorithmName", values="withinOpt")
-    pivot = pivot[~pivot.index.duplicated(keep='first')]
-
-    palette = sns.color_palette(n_colors=10)
-    plot = pivot.plot(color=palette, title=plot_title, legend=True, yerr=errors, ecolor='black', elinewidth=1,
-                      capsize=4, capthick=1)
-
-    # plot.set_xscale('log')
-    # plot.set_yscale('log')
-
-    # plot.set_xticks([50, 100, 150, 250, 500, 1000, 2000, 3200])
-    plot.set_xticks(data.actionDuration.unique())
-    plot.get_xaxis().set_major_formatter(mpl.ticker.ScalarFormatter())
-
-    plot.get_yaxis().set_major_formatter(mpl.ticker.ScalarFormatter())
-
-    plot.set_xlabel('Planning Time per Iteration (expansions)')
-    plot.set_ylabel('Goal Achievement Time (Factor of Optimal)')
-    plot.legend(title="")
-
-    pdf = PdfPages("../output/" + plot_title + ".pdf")
-    plt.savefig(pdf, format='pdf')
-    pdf.close()
-
-
-def main(individual_plots, paths_to_base, paths, title, domain_token):
-    set_rc()
-
-    results = []
-    for path_name in paths:
-        results += read_data(path_name)
-
-    base_results = []
-    if paths_to_base is not None:
-        for base_path_name in paths_to_base:
-            base_results += read_data(base_path_name)
-
-    data = construct_data_frame(results)
-
-    compact_base_data = construct_data_frame(base_results)
-
-    print(len(base_results))
-
-    remove_unused_columns(data)
-    remove_unused_columns(compact_base_data)
-
-    action_durations = data.actionDuration.unique()
-    print(f'action duration used: {action_durations}')
-
-    data = pd.concat([data, compact_base_data], sort=False)
-
-    
-    data = data[~data['errorMessage'].notnull()]
-    
-    print(len(data[data.algorithmName == 'WEIGHTED_A_STAR']))
-
-    
-    # drop certain rows for brevity
-    data = data[~(data['tbaOptimization'] == 'NONE')]
-    
-    if domain_token is not None:
-        data = data[data['domainPath'].str.contains(domain_token)]
-    
-    data = extrapolate_within_optimal(data)
-
-    data = data[~(data.algorithmName == 'WEIGHTED_A_STAR')]
-
-    # df = data.groupby(['algorithmName', 'expansionDelay'], as_index=False).mean()
-
-    # print(df)
-    # df.plot(x='expansionDelay', y='withinOpt')
-    # plt.show()
-
-    if individual_plots:
-        plot_domain_instances(data)
+    if domain_tokens is None:
+        return get_within_opt_results(data, group_by)
     else:
-        plot_all_experiments(data, title)
+        results = {}
+        for domain in domain_tokens:
+            domain_data = data[data['domainPath'].str.contains(domain)]
+            results[domain] = get_within_opt_results(domain_data, group_by)
 
-
-def remove_unused_columns(data):
-    data.drop(['actions', 'commitmentType', "success", "timeLimit",
-               "terminationType", 'timestamp', 'octileMovement', 'lookaheadType',
-               'firstIterationDuration', 'generatedNodes', 'expandedNodes',
-               "targetSelection", "safetyExplorationRatio", "safetyProof", "safetyWindowSize", "safetyBackup",
-               'domainSeed', 'averageVelocity', "proofSuccessful", "rawDomain", "anytimeMaxCount",
-               "systemProperties", "towardTopNode", "weight", "numberOfProofs"],
-              axis=1,
-              inplace=True,
-              errors='ignore')
-
-
-def extrapolate_within_optimal(data):
-    astar = data[data["algorithmName"] == "WEIGHTED_A_STAR"]
-
-    astar["optimalPathLength"] = astar["pathLength"]
-    astar = astar[["domainPath", "optimalPathLength"]]
-
-    data = pd.merge(data, astar, how='inner', on=["domainPath"])
-    data["withinOpt"] = data["goalAchievementTime"] / (data["actionDuration"] * data["optimalPathLength"])
-
-    return data
-
-
-if __name__ == "__main__":
-    # define command line usage
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-b", "--paths_to_base", nargs="*", help="Path to base results JSON")
-    parser.add_argument("-p", "--paths", nargs="*", help="Path to experiment results JSON",
-                        default=["../output/results.json"])
-    parser.add_argument("-i", "--individual",
-                        help="Should plots be generated for each domain individually? (Primarily for debugging)",
-                        action="store_true")
-    parser.add_argument("-t", "--title", help="Title for plot (ignored for individual plots)", default="Experiments")
-    parser.add_argument("-d", "--domain_token", help="Domain token for filtering")
-
-    args = parser.parse_args()
-    individual_plots = args.individual
-    paths_to_base = args.paths_to_base
-    paths = args.paths
-    title = args.title
-    domain_token = args.domain_token
-    
-    main(individual_plots, paths_to_base, paths, title, domain_token)
+        return results
