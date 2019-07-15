ce50835d8900d27856633c4b6c615b0316a57503
William Doyle <williamdoyleiii@gmail.com>
2019-07-15 09:13:43 -0400

    lazy config update

--- src/main/kotlin/edu/unh/cs/searkt/Configurations.kt
+++ src/main/kotlin/edu/unh/cs/searkt/Configurations.kt
@@ -16,20 +16,20 @@ import java.util.concurrent.TimeUnit
  */
 fun generateConfigurations(): String {
     val domains = mutableListOf<Pair<Domains, String>>(
-//            Domains.RACETRACK to "input/racetrack/uniform.track",
-//            Domains.RACETRACK to "input/racetrack/hansen-bigger.track"
+            Domains.RACETRACK to "input/racetrack/uniform.track",
+            Domains.RACETRACK to "input/racetrack/hansen-bigger.track"
 //            ,Domains.RACETRACK to "input/racetrack/hansen-bigger-octa.track"
     )
 
 //    domains += (0..33).map { Domains.PANCAKE to "input/pancake/$it.pqq" }
 //    domains += (0..33).map { Domains.HEAVY_PANCAKE to "input/pancake/$it.pqq" }
 
-    domains += (1..100).map { Domains.SLIDING_TILE_PUZZLE_4 to "input/tiles/korf/4/real/$it" }
+//    domains += (1..100).map { Domains.SLIDING_TILE_PUZZLE_4 to "input/tiles/korf/4/real/$it" }
 //    domains += (1..33).map { Domains.SLIDING_TILE_PUZZLE_4_INVERSE to "input/tiles/korf/4/real/$it" }
-    domains += (1..100).map { Domains.SLIDING_TILE_PUZZLE_4_HEAVY to "input/tiles/korf/4/real/$it" }
+//    domains += (1..100).map { Domains.SLIDING_TILE_PUZZLE_4_HEAVY to "input/tiles/korf/4/real/$it" }
 
-//    domains += (0..10).map { Domains.GRID_WORLD to "input/vacuum/orz100d/orz100d.map_scen_$it" }
-//    domains += (0..10).map { Domains.LIFE_GRIDS to "input/lifegrids/lifegrids$it.lg" }
+    domains += (0..10).map { Domains.GRID_WORLD to "input/vacuum/orz100d/orz100d.map_scen_$it" }
+    domains += (0..10).map { Domains.LIFE_GRIDS to "input/lifegrids/lifegrids$it.lg" }
 
 //    domains += (0..10).map { Domains.VACUUM_WORLD to "input/vacuum/gen/vacuum$it.vw" }
 //    domains += (0..10).map { Domains.HEAVY_VACUUM_WORLD to "input/vacuum/gen/vacuum$it.vw" } //    val daoMapNames = listOf("arena", "arena2", "brc000d", "brc100d", "brc101d", "brc200d", "brc201d", "brc202d", "brc203d", "brc204d", "brc300d", "brc501d", "brc502d", "brc503d", "brc504d", "brc505d", "brc997d", "brc999d", "combat", "combat2", "den000d", "den001d", "den005d", "den009d", "den011d", "den012d", "den020d", "den101d", "den200d", "den200n", "den201d", "den202d", "den203d", "den204d", "den206d", "den207d", "den308d", "den312d", "den400d", "den401d", "den403d", "den404d", "den405d", "den407d", "den408d", "den500d", "den501d", "den502d", "den504d", "den505d", "den510d", "den520d", "den600d", "den601d", "den602d", "den900d", "den901d", "den998d", "hrt000d", "hrt001d", "hrt002d", "hrt201d", "hrt201n", "isound1", "lak100c", "lak100d", "lak100n", "lak101d", "lak102d", "lak103d", "lak104d", "lak105d", "lak106d", "lak107d", "lak108d", "lak109d", "lak110d", "lak200d", "lak201d", "lak202d", "lak203d", "lak250d", "lak300d", "lak302d", "lak303d", "lak304d", "lak307d", "lak308d", "lak400d", "lak401d", "lak403d", "lak404d", "lak405d", "lak503d", "lak504d", "lak505d", "lak506d", "lak507d", "lak510d", "lak511d", "lak512d", "lak513d", "lak514d", "lak515d", "lak519d", "lak526d", "lgt101d", "lgt300d", "lgt600d", "lgt601d", "lgt602d", "lgt603d", "lgt604d", "lgt605d", "orz000d", "orz100d", "orz101d", "orz102d", "orz103d", "orz105d", "orz106d", "orz107d", "orz200d", "orz201d", "orz203d", "orz300d", "orz301d", "orz302d", "orz303d", "orz304d", "orz500d", "orz601d", "orz700d", "orz701d", "orz702d", "orz703d", "orz704d", "orz800d", "orz900d", "orz901d", "orz999d", "ost000a", "ost000t", "ost001d", "ost002d", "ost003d", "ost004d", "ost100d", "ost101d", "ost102d", "oth000d", "oth001d", "oth999d", "rmtst", "rmtst01", "rmtst03")
@@ -46,16 +46,16 @@ fun generateConfigurations(): String {
             // There are 2000+ instances in each scen file
             Triple(Domains.GRID_MAP, Configurations.DOMAIN_SEED.toString(), 0..2000L step 500)
     )
-     val plannerExtras = listOf(
-             Triple(Planners.BOUNDED_SUBOPTIMAL_EXPLORATION, "embeddedAlgorithm",
-                     listOf(
-                             Planners.OPTIMISTIC,
-                             Planners.GREEDY,
-                             Planners.SPEEDY,
-                             Planners.WEIGHTED_A_STAR_XDP
-                           )
-             )
-     )
+//     val plannerExtras = listOf(
+//             Triple(Planners.BOUNDED_SUBOPTIMAL_EXPLORATION, "embeddedAlgorithm",
+//                     listOf(
+//                             Planners.OPTIMISTIC,
+//                             Planners.GREEDY,
+//                             Planners.SPEEDY,
+//                             Planners.WEIGHTED_A_STAR_XDP
+//                           )
+//             )
+//     )
 
     var configurations = edu.unh.cs.searkt.experiment.configuration.generateConfigurations(
             domains = domains,
@@ -63,14 +63,15 @@ fun generateConfigurations(): String {
 //                    Planners.WEIGHTED_A_STAR,
 //                    Planners.EES,
 //                    Planners.DPS,
-                    Planners.BOUNDED_SUBOPTIMAL_EXPLORATION
+//                    Planners.BOUNDED_SUBOPTIMAL_EXPLORATION
+                      Planners.SUBPOTENTIAL
 //                    Planners.WEIGHTED_A_STAR_XDP,
 //                    Planners.WEIGHTED_A_STAR_XUP
             ),
 //            planners = listOf(Planners.WEIGHTED_A_STAR),
             actionDurations = listOf(1),
             timeLimit = timeLimit,
-            plannerExtras = plannerExtras,
+//            plannerExtras = plannerExtras,
             domainExtras = domainExtras
     )
 
